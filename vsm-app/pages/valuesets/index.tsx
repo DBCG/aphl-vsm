44e3534079bcd6d6ca997f97e662dc3ff3c45d65
Junaid Siddiqui <junaid.wim@gmail.com>
2022-03-14 15:30:18 -0400

    receiving codesystems and valuesets from VSAC

--- vsm-app/pages/valuesets/index.tsx
+++ vsm-app/pages/valuesets/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import type { Bundle } from 'fhir/r4'
 import styled from 'styled-components'
 import { PageTitle } from '../../components/Typography'
 import { SearchInput } from '../../components/SearchInput'
@@ -18,16 +19,46 @@ const Col = styled.div`
   height: fit-content;
 `
 
-const ValueSets: NextPage = () => {
+interface ValueSetProps {
+  vsacValueSet: Bundle
+}
+
+const ValueSets: NextPage<ValueSetProps> = ({ vsacValueSet }) => {
   return (
     <Col>
       <PageTitle>ValueSet Search</PageTitle>
       <Row>
         <SearchInput placeholder='Search by ID, Name, Title' />
         <Button text='Add New Program' />
+        { JSON.stringify(vsacValueSet, null, 2) }
       </Row>
     </Col>
   )
+}
+
+export async function getStaticProps() {
+  const { VSAC_USERNAME, VSAC_API_KEY } = process.env
+  const vsacBaseUrl = 'https://cts.nlm.nih.gov/fhir'
+
+  const headers = new Headers();
+  const authString = `${VSAC_USERNAME}:${VSAC_API_KEY}`
+  headers.set('Authorization', `Basic ${Buffer.from(authString).toString('base64')}`)
+  const fetchOptions = { method: 'GET', headers }
+
+  const responses = await Promise.all([
+    fetch(`${vsacBaseUrl}/ValueSet`, fetchOptions ),
+    fetch(`${vsacBaseUrl}/CodeSystem`, fetchOptions)
+  ])
+  
+  console.log(await responses[0].json())
+
+  const vsacValueSet = {};
+
+  return {
+    props: {
+      vsacValueSet
+    }
+  }
 }
 
 export default ValueSets
