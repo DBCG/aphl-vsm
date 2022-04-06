7f142739902654ce0f52edc508b27f360aaa279d
Junaid Siddiqui <junaid.wim@gmail.com>
2022-04-06 12:08:49 +0100

    handle loading better to account for fetch timeout in api better ability to demo without page refresh

--- vsm-app/pages/valuesets/index.tsx
+++ vsm-app/pages/valuesets/index.tsx
@@ -1,16 +1,19 @@
 import { Bundle, ValueSet } from 'fhir/r4'
-import { SyntheticEvent, useEffect, useState } from 'react'
+import { ChangeEvent, SyntheticEvent, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { PageTitle } from '../../components/Typography'
 import { SearchInput } from '../../components/SearchInput'
 import { SearchTable } from '../../components/SearchTable'
 import { CodeSystemFilters } from '../api/codesystem/filters'
+import { Button } from '../../components/buttons/Button'
+import LoadingIndicator from '../../components/LoadingIndicator'
 
 const Row = styled.div`
   display: flex;
   flex: 1;
   flex-direction: row;
   justify-content: space-between;
+  margin-bottom: 1rem;
 `
 
 const Col = styled.div`
@@ -27,15 +30,12 @@ interface ValueSetsAndFilters {
 
 type SearchType = 'name' | 'oid' | 'steward'
 
-type SearchEvent = EventTarget & {
-  search: Record<string, string>
-}
-
 const ValueSets = () => {
   const [valueSetsAndFilters, setValueSetsAndFilters] = useState<ValueSetsAndFilters>({
-    valueSets: [],
+    valueSets: undefined,
     filters: []
   })
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
     async function getValueSetsAndFilters(): Promise<void> { // this gets the initial chart items for the page
@@ -43,15 +43,18 @@ const ValueSets = () => {
         fetch(`api/valueset`),
         fetch(`api/codesystem/filters`)
       ])
-
-      const { entry } = await responses[0].json()
-      setValueSetsAndFilters({
-        valueSets: entry,
-        filters: await responses[1].json()
-      })
+      if(responses[0].ok) {
+        const { entry } = await responses[0].json()
+        setValueSetsAndFilters({
+          valueSets: entry,
+          filters: await responses[1].json()
+        })
+      } else {
+        setValueSetsAndFilters({ ...valueSetsAndFilters, valueSets: []});
+      }
     }
     void getValueSetsAndFilters()
-  }, [])
+  })
 
   /**
    *  When a user clicks the search button, an API call is made to the `/search` endpoint to query by name/OID/steward
@@ -60,11 +63,10 @@ const ValueSets = () => {
     event.preventDefault()
     const oidRegex = new RegExp('^([0-2])((\.0)|(\.[1-9][0-9]*))*$')
     let type: SearchType = 'name';
-    const { search: { value } } = event.target as SearchEvent
     
-    if (oidRegex.test(value)) { type = 'oid'}
+    if (oidRegex.test(searchTerm)) { type = 'oid'}
 
-    const response = await fetch(`api/valueset/search?}&search=${value}&searchType=${type}`)
+    const response = await fetch(`api/valueset/search?}&search=${searchTerm}&searchType=${type}`)
 
     if (type === 'oid') { // an OID search returns a single ValueSet that needs to be handled uniquely for the SearchTable component
       const valueSetResponse = await response.json() as ValueSet
@@ -76,20 +78,23 @@ const ValueSets = () => {
   }
 
   const { valueSets } = valueSetsAndFilters
-  const allowInput = (valueSets && valueSets.length > 0)
-
-  // Currently using simple HTML form tags until styled components created
   return (
     <Col>
       <PageTitle>ValueSet Search</PageTitle>
       <Row>
-        <form onSubmit={submitSearch}>
-          {
-            allowInput ? <input name="search" type="text" placeholder='Search by Name, OID' required/> :
-              <input name="search" type="text" placeholder='Search by Name, OID' disabled required/>
-          }
-          <button type="submit">Search</button>
-        </form>
+        { valueSets? <>
+            <SearchInput
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+              id='program-search'
+              label='Search by Name, OID'
+              hasIcon={true}
+              minWidth={400}
+            />
+            <Button
+              text='Submit Search'
+              onClick={submitSearch}
+            />
+          </>: <LoadingIndicator /> }
       </Row>
       <SearchTable valueSets={valueSets} />
     </Col>
