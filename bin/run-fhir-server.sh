10a0e90b84276e90254a2a06f5908267ac0a92e6
Vicki Strateman <vicki@vermonster.com>
2022-04-12 14:06:54 -0400

    make sure docker volumes are deleted + remounted on server start

--- bin/run-fhir-server.sh
+++ bin/run-fhir-server.sh
@@ -2,6 +2,12 @@
 # exit when any command fails
 set -e
 
+# stop container by the full image name
+docker stop $(docker ps -q --filter ancestor=hapiproject/hapi:v5.6.0)
+# delete the volume, or else you get stale data
+docker volume rm -f hapi-dev-vsm-app
+# create a new volume
 docker volume create hapi-dev-vsm-app 2>/dev/null
 
+# run hapi server with attached volume
 docker run -d --rm -p 8080:8080 --mount source=hapi-dev-vsm-app,target=/usr/local/tomcat/target/database hapiproject/hapi:v5.6.0
