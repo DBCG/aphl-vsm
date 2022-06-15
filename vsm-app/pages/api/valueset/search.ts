1cc91d4108b535a6e3629255ab836e9db437fa40
Vicki Strateman <vicki@vermonster.com>
2022-06-15 15:29:49 -0400

    adjust searh api to handle malformed oids, send back detailed errors

--- vsm-app/pages/api/valueset/search.ts
+++ vsm-app/pages/api/valueset/search.ts
@@ -1,39 +1,85 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { vsacFhirClient } from 'fhirClients'
 
+interface FetchError {
+  errorType: 'server-error' | 'failed-oids' | '',
+  message: string
+}
+
+interface Response {
+  valueSets: fhir4.ValueSet[] | [],
+  error?: FetchError
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<any> {
   if (req.method === 'GET') {
     const { search, searchType } = req.query
+    let responseInfo: Response = {
+      valueSets: []
+    }
 
     try {
-      let response;
+      let serverResponse
       switch (searchType) {
         case 'name':
-          response = await vsacFhirClient.search({
-            resourceType: 'ValueSet', searchParams: { 'name:contains': search }
-          })
+          try {
+            serverResponse = await vsacFhirClient.search({
+              resourceType: 'ValueSet', searchParams: { 'name:contains': search }
+            })
+
+            if (serverResponse.entry) {
+              responseInfo.valueSets = serverResponse.entry.map((item) => (
+                item.resource
+              ))
+            }
+          } catch (e) {
+            console.error(e)
+            responseInfo.error = {
+              errorType: 'server-error',
+              message: `Search for '${search}' failed.`
+            }
+          }
           break
         case 'oid':
           // @ts-ignore-next-line
-          const oidList = search?.split(',')
-          response = await Promise.all(oidList.map((oid: string) => (
-            vsacFhirClient.read({
-              resourceType: 'ValueSet', id: oid
-            })
-          )))
+          const oidList: string[] = search?.split(',')
+          try {
+            serverResponse = await Promise.allSettled(oidList.map((oid: string) => (
+              vsacFhirClient.read({
+                resourceType: 'ValueSet', id: oid
+              })
+            )))
+
+            responseInfo.valueSets = serverResponse
+              ?.map(item => item?.status === 'fulfilled' && item?.value)
+              ?.filter(x => x) as fhir4.ValueSet[]
+
+            const successfulOIDs = responseInfo?.valueSets?.map(v => v?.id)
+
+            const failedOIDs = oidList?.filter((oid) => successfulOIDs?.includes(oid))
+
+            if (failedOIDs.length > 0) {
+              responseInfo.error = {
+                errorType: 'failed-oids',
+                message: `Search for these OIDs failed: ${failedOIDs.join(', ')}.`
+              }
+            }
+
+          } catch (e) {
+            console.error(e)
+            responseInfo.error = {
+              errorType: 'server-error',
+              message: `Search by OID failed.`
+            }
+          }
 
           break
-        // not currently using steward as a search
-        case 'steward':
-          response = await vsacFhirClient.search({
-            resourceType: 'ValueSet', searchParams: { 'publisher:contains': search }
-          })
       }
 
-      res.status(200).send(response)
+      res.status(200).send(responseInfo)
     } catch (e) {
       console.error('error:  ', e)
       res.status(400).json({ error: 'Loading ValueSets failed' })
