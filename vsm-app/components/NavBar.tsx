a58243f4294c842c7391e8ce544e62aeff5e2fbc
Vicki Strateman <vicki@vermonster.com>
2022-07-19 11:36:37 -0400

    add breadcrumb component

--- vsm-app/components/NavBar.tsx
+++ vsm-app/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Breadcrumbs from 'nextjs-breadcrumbs'
 import { signOut } from 'next-auth/react'
 import { Button } from './buttons/Button'
 
@@ -14,7 +15,7 @@ const Bar = styled.ol`
   list-style-type: none;
   margin: 0;
   padding: 15px;
-  justify-content: flex-end;
+  justify-content: space-between;
   display: flex;
   flex: 1;
   width: 100%;
@@ -25,6 +26,16 @@ const NavBar = () => {
   return (
     <BarWrapper>
       <Bar>
+      <Breadcrumbs
+        useDefaultStyle
+        omitRootLabel
+        activeItemClassName='active-crumb'
+        inactiveItemClassName='inactive-crumb'
+        transformLabel={(title) => title.charAt(0).toUpperCase() + title.slice(1)}
+        activeItemStyle={{
+          backgroundColor: 'red !important'
+        }}
+      />
         <Button text='Sign Out' onClick={() => signOut()}/>
       </Bar>
     </BarWrapper>
