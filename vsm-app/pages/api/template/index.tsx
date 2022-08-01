8ae7013b02c630b1e91d2c4f4d3cc051def1ff3a
Hayes Murdock <hayes@alphora.com>
2022-08-01 16:38:36 -0600

    APHL-303 Backend updates for operation

--- vsm-app/pages/api/template/index.tsx
+++ vsm-app/pages/api/template/index.tsx
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { fhirCdrClient } from 'fhirClients'
+//import { fhirCdrClient } from 'fhirClients'
 
 export default async function handler(
   req: NextApiRequest,
