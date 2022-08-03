e33f7ebf7291db680d33e94fa08bceda9b547e4b
Vicki Strateman <vicki@vermonster.com>
2022-08-03 11:32:04 -0400

    add cleanup script for docker to simplify restarts without persisting config

---
+++ bin/docker-cleanup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+KEYCLOAK_DIR=${DIR}/../keycloak
+
+echo -e "This command will stop + delete \nall containers, \ntheir associated volumes, \nand docker networks"
+read -p "Continue? (y/n)?" choice
+
+
+case "$choice" in 
+  y|Y ) cd ${KEYCLOAK_DIR} \
+    && docker-compose down \
+    && docker rm -f $(docker ps -a -q) \
+    && docker volume rm $(docker volume ls -q) \
+    && docker network prune;;
+  n|N ) echo "cancelled";;
+  * ) echo "invalid option";;
+esac
+
+cd ${DIR}/..
+
