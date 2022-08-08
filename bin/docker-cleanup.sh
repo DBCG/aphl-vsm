b950f5d24815759b7f09f6704ace2ce2178c61c4
Vicki Strateman <vicki@vermonster.com>
2022-08-08 10:57:22 -0400

    edit cleanup script

--- bin/docker-cleanup.sh
+++ bin/docker-cleanup.sh
@@ -5,7 +5,7 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 KEYCLOAK_DIR=${DIR}/../keycloak
 
-echo -e "This command will stop + delete: \n  - all docker containers \n  - associated volumes \n  - docker networks\n"
+echo -e "This command will stop + delete: \n  - all containers \n  - associated volumes \n  - docker networks\n"
 read -p "Continue? (y/n)?" choice
 
 
