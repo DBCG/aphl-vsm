5634e59e20a9711f67a2bc66777f6375c33275fd
Vicki Strateman <vicki@vermonster.com>
2022-08-08 12:53:31 -0400

    update configuration file and docker container creation

--- bin/docker-cleanup.sh
+++ bin/docker-cleanup.sh
@@ -8,12 +8,13 @@ KEYCLOAK_DIR=${DIR}/../keycloak
 echo -e "This command will stop + delete: \n  - all containers \n  - associated volumes \n  - docker networks\n"
 read -p "Continue? (y/n)?" choice
 
+ALL_CONTAINERS=$(docker ps -a -q)
 
 case "$choice" in 
   y|Y ) cd ${KEYCLOAK_DIR} \
     && docker-compose down \
-    && docker rm -f $(docker ps -a -q) \
-    && docker volume rm $(docker volume ls -q) \
+    && docker rm -f $ALL_CONTAINERS \
+    && docker volume prune \
     && docker network prune;;
   n|N ) echo "cancelled";;
   * ) echo "invalid option";;
