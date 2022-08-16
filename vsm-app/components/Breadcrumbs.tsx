5d066a815e94ead8e1cd2ae7e159671f7920a2c6
Vicki Strateman <vicki@vermonster.com>
2022-08-16 15:44:16 -0400

    style breadcrumbs

--- vsm-app/components/Breadcrumbs.tsx
+++ vsm-app/components/Breadcrumbs.tsx
@@ -6,19 +6,30 @@ import styled from 'styled-components'
 const NavList = styled.ul`
   display: flex;
   flex-direction: row;
+  & > :last-child {
+    font-weight: bold;
+  }
 `
 
 const NavItem = styled.li`
+  display: inline-block;
   list-style-type: none;
-  margin: 4px;
-  padding: 3px;
   background-color: white;
+  text-transform: capitalize;
+  font-weight: 400;
+  padding: 8px 20px;
+  background-color: rgba(192, 231, 235, 1);
+  margin: auto;
+  color: var(--theme-500);
+  clip-path: polygon(0 0, calc(100% - 10px) 0, 100% 50%, calc(100% - 10px) 100%, 0 100%, 10px 50%);
+  &:hover {
+    background-color: #ebdac0;
+    cursor: pointer;
+  }
 `
 
 const composePath = (pathItems: string[], lastOfPath: string) => {
-  console.log('test')
   const idx = pathItems.indexOf(lastOfPath)
-  console.log('pathitems: ', pathItems)
   return pathItems.slice(0, idx + lastOfPath.length)
 }
 
@@ -29,7 +40,6 @@ const BreadCrumbs = () => {
   useEffect(() => {
     if (router) {
       const crumbs = router.asPath.split('/')
-      console.log('crumbs: ', crumbs)
       setBreadCrumbs(crumbs)
     } else {
       setBreadCrumbs([])
@@ -38,17 +48,16 @@ const BreadCrumbs = () => {
 
   if (!breadCrumbs.length) return null
 
-  const items = breadCrumbs.map(c => {
+  const items = breadCrumbs.map((c, index) => {
     if (c !== '') {
       return (
-      <NavItem key={c}>
-        <Link href={composePath(router.asPath, c)}>
-          {`${c}`} 
-        </Link>
-      </NavItem>
-    )
-    }
-})
+      <Link href={composePath(router.asPath, c)}>
+        <NavItem alpha={index / 0.1} key={c}>
+            {`${c}`} 
+        </NavItem>
+      </Link>
+    )}
+  })
 
   return (
     <NavList>
