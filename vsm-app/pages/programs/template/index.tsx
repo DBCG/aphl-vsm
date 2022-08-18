a41bc4e950a118eec4a93e4bcd3c4ae789143acc
Vicki Strateman <vicki@vermonster.com>
2022-08-18 00:19:05 -0400

    simplify call to program to use existing functions

--- vsm-app/pages/programs/template/index.tsx
+++ vsm-app/pages/programs/template/index.tsx
@@ -1,12 +1,8 @@
-import { useMemo, useEffect, useState } from 'react';
-import LoadingIndicator from "@/components/LoadingIndicator";
-import Router from 'next/router';
+import { useMemo, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
-import { resourceLimits } from 'worker_threads';
 import DT from 'react-data-table-component';
-import { IconButton } from '@/components/buttons/IconButton';
-import { fetchData } from 'next-auth/client/_utils';
+import { useGetProgramValueSetDetails } from '@/hooks/useGetProgramValueSetDetails';
 
 const Row = styled.div`
   display: flex;
@@ -22,9 +18,6 @@ const Col = styled.div`
   flex-direction: column;
   height: fit-content;
 `
-const ButtonWrapper = styled.div`
-  margin-left: 6px;
-`
 
 const customStyles = {
   cells: {
@@ -36,12 +29,16 @@ const customStyles = {
 }
 
 const Template = () => {
-  const [libraryData, setLibraryData] = useState(null)
-  const [libraryTemplateData, setLibraryTemplateData] = useState(null)
-  const [loading, setLoading] = useState(true)
-  //const newLibraryData = {data: 'hello from the frontend!'}
-  let jsonData: any = '';
+
   const router = useRouter()
+  
+  let programId = router?.query?.id || ''
+  const program = useGetProgramValueSetDetails(programId)
+
+  useEffect(() => {
+    console.log('program: ', program)
+  }, [program])
+
   let href: any = '';
   
   const handleClick = () => {
@@ -50,104 +47,31 @@ const Template = () => {
 
   const columns = useMemo(() => [
     {
-      name: 'Updated',
-      selector: (row: fhir4.Library) => row.date,
+      name: 'Value Set Name',
+      selector: (row) => row.valueSet.name,
       sortable: true,
-      maxWidth: '150px',
+      maxWidth: '350px',
       wrap: true
     },
     {
-      name: 'ID',
-      selector: (row: fhir4.Library) => row.id,
+      name: 'Template Value Set Version',
+      selector: (row) => row.valueSet.version,
       sortable: true,
       maxWidth: '250px',
       wrap: true,
       
     },
     {
-      name: 'Name',
-      selector: (row: fhir4.Library) => row.name,
+      name: 'Edit Value Set Version',
+      selector: (row) => 'include a select here to get available versions?',
       sortable: true,
-      maxWidth: '300px',
-      wrap: true
-    },
-    {
-      name: 'Title',
-      selector: (row: fhir4.Library) => row.title,
-      sortable: true,
-      maxWidth: '200px',
-      wrap: true
-    },
-    {
-      name: 'Description',
-      selector: (row: fhir4.Library) => row.description,
-      sortable: false,
-      maxWidth: '300px',
-      minWidth: '300px',
-      wrap: true
-    },
-    {
-      name: 'Version',
-      selector: (row: fhir4.Library) => row.version,
-      sortable: true,
-      wrap: true
-    },
-    {
-      name: 'Details',
-      selector: (row: fhir4.Library) => row.name,
-      sortable: false,
+      maxWidth: '250px',
       wrap: true,
-      cell: (row: fhir4.Library) => (
-        <ButtonWrapper>
-          <IconButton
-            onClick={() => router.push(`/programs/${row.id}`)}
-            buttonContext='edit'
-          />
-        </ButtonWrapper>
-      )
+      
     }
   ], [])
 
-  useEffect(() => {
-    setLoading(true)
-
-    async function searchForLibrary() {
-      const result = await fetch('../api/programs', {
-        method: 'GET',
-        //body: JSON.stringify(newLibraryData)
-      })
-    
-      //todo - get json data from result and pass it to getData function to send it to the backend.
-      setLibraryData(result)
-      console.log('in programs template: ' + result.body)
-    }
-
-    jsonData = searchForLibrary()
-    console.log('jsonData: ' + JSON.stringify(jsonData))
-    setLoading(false)
-  }, [])
-
-  useEffect(() => {
-
-    async function getTemplateData() {
-      const asyncTemplateData = await fetch('../api/template', {
-        method: 'POST',
-        //body: JSON.stringify(libraryData)
-      })
-
-      jsonData = await asyncTemplateData.json()
-      console.log('template data: ' + jsonData)
-      setLibraryTemplateData(jsonData)
-      
-    }
-
-    getTemplateData()
-    setLoading(false)
-  }, [])  //[libraryData])
-
-  if (loading) {
-    return <LoadingIndicator/>
-  } else {
+  {
     return (
       <Col>
         <Row>
@@ -155,19 +79,16 @@ const Template = () => {
             Return to programs page
           </a>
         </Row>
-       <Row>
-
-       </Row>
-       <DT
-        data={jsonData}
-         // @ts-expect-error
-         columns={columns}
-         theme='aphl'
-         pagination
-         fixedHeader
-         customStyles={customStyles}
-       />
-       </Col>
+        <p>Edit Program Value Sets</p>
+        <DT
+          data={program?.data}
+          columns={columns}
+          theme='aphl'
+          pagination
+          fixedHeader
+          customStyles={customStyles}
+        />
+      </Col>
     )
   }
 }
