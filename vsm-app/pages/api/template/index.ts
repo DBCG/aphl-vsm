ec41348d2ab5885ff07a1017b9c0b01bd4acab7b
Hayes Murdock <hayes@alphora.com>
2022-08-24 09:53:49 -0600

    APHL-303 Updated logic for UI routing and data

--- vsm-app/pages/api/template/index.ts
+++ vsm-app/pages/api/template/index.ts
@@ -1,10 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { fhirCdrClient } from 'fhirClients';
-import { Router, useRouter } from 'next/router';
 
-
-//const router = useRouter()
 // this code ingests a FHIR Library, and will POST a modified clone as a template
 export default async function handler(
   req: NextApiRequest,
@@ -16,22 +13,18 @@ export default async function handler(
     let bodyObj = JSON.parse(req.body)
     // deleting mutates the object, deleting the id key
     delete bodyObj.id
-    const res = await fhirCdrClient.create({
+    delete bodyObj.version
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+    bodyObj.version = 'draft-' + date;
+    const response = await fhirCdrClient.create({
       resourceType: 'Library',
       body: req.body,
     })
 
-    if(res.status(200)) {
-      //router.push(`/programs/${id}`)
-    } else {
-      res.status(400).json({ error: 'Creation of new library failed.' })
-    }
+    res.send(response);
   } catch (e: any) {
     console.error('error:  ', e)
     res.status(400).json({ error: 'Creation of new library failed.' })
   }
 }
-
-//export async function getStaticProps() {
-//
-//}
