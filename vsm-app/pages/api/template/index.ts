acc056f3cdc2ebe411f08054b3af4d9ce2ef83b5
Vicki Strateman <vicki@vermonster.com>
2022-09-12 13:15:54 -0400

    edit the way version is composed, make sure cloned program status is draft

--- vsm-app/pages/api/template/index.ts
+++ vsm-app/pages/api/template/index.ts
@@ -8,21 +8,23 @@ export default async function handler(
   res: NextApiResponse
 ): Promise<any> {
   // create library template
-  
+
   try {
     let bodyObj = JSON.parse(req.body)
-    // deleting mutates the object, deleting the id key
+    console.log('body Obj: ', bodyObj)
+    // deleting mutates the object, deleting the ids key
     delete bodyObj.id
     delete bodyObj.version
+    bodyObj.status = 'draft'
     const current = new Date();
-    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-    bodyObj.version = 'draft-' + date;
+    const date = `${current.getFullYear()}-${current.getMonth() + 1}-${current.getDate()}`
+    bodyObj.version = date
     const response = await fhirCdrClient.create({
       resourceType: 'Library',
       body: bodyObj,
     })
-
-    res.send(response);
+    console.log('response: ', response)
+    res.send(response)
   } catch (e: any) {
     console.error('error:  ', e)
     res.status(400).json({ error: 'Creation of new library failed.' })
