9994f45a59da3652f00d08fa8ebb795614755529
Vicki Strateman <vicki@vermonster.com>
2022-09-20 12:51:02 -0400

    create two structures to hold client data

--- vsm-app/fhirClientOptions.ts
+++ vsm-app/fhirClientOptions.ts
@@ -1,6 +1,37 @@
-const terminologyServerEndpoints = [
-  { label: 'VSAC', dataId: 'VSAC', value: { title: 'vsac', url: process.env.NEXT_PUBLIC_VSAC_BASE_URL } },
-  { label: 'Ontoserver (R4)', dataId: 'ontoserverR4', value: { title: 'ontoserverR4', url: process.env.NEXT_PUBLIC_ONTOSERVER_R4_BASE_URL } }
+// does not include username/password for FE use (building dropdown, etc)
+const terminologyServerEndpoints_FE = [
+  {
+    label: 'VSAC',
+    dataId: 'vsac',
+    value: {
+      url: process.env.NEXT_PUBLIC_VSAC_BASE_URL,
+      auth: true
+    }
+  },
+  {
+    label: 'Ontoserver (R4)',
+    dataId: 'ontoserver_r4',
+    value: {
+      url: process.env.NEXT_PUBLIC_ONTOSERVER_R4_BASE_URL,
+      auth: false
+    }
+  }
 ]
 
-export { terminologyServerEndpoints }
+// includes username/password to call terminology client in server
+const terminologyServerEndpoints_BE = [
+  {
+    id: 'vsac',
+    url: process.env.NEXT_PUBLIC_VSAC_BASE_URL,
+    auth: {
+      user: process.env.VSAC_USERNAME,
+      pw: process.env.VSAC_API_KEY
+    }
+  },
+  {
+    id: 'ontoserver_r4',
+    url: process.env.NEXT_PUBLIC_ONTOSERVER_R4_BASE_URL
+  }
+]
+
+export { terminologyServerEndpoints_FE, terminologyServerEndpoints_BE }
