d0b406aeb8042a9044c8363bb99483e8ae4e167c
Vicki Strateman <vicki@vermonster.com>
2022-09-28 10:24:44 -0400

    use flag to hide dev servers

--- vsm-app/fhirClientOptions.ts
+++ vsm-app/fhirClientOptions.ts
@@ -1,8 +1,13 @@
 const ONTOSERVER_R4_BASE_URL = 'https://r4.ontoserver.csiro.au/fhir'
 
-const terminologyServerEndpoints = [
+let terminologyServerEndpoints = [
   { label: 'VSAC', dataId: 'VSAC', value: { title: 'vsac', url: process.env.NEXT_PUBLIC_VSAC_BASE_URL } },
-  { label: 'Ontoserver (R4)', dataId: 'ontoserverR4', value: { title: 'ontoserverR4', url: ONTOSERVER_R4_BASE_URL } }
 ]
+
+if (process.env.NEXT_PUBLIC_SHOW_TEST_TERMINOLOGY_SERVER) {
+  terminologyServerEndpoints.push(
+    { label: 'Ontoserver (R4)', dataId: 'ontoserverR4', value: { title: 'ontoserverR4', url: ONTOSERVER_R4_BASE_URL } }
+  )
+}
 
 export { terminologyServerEndpoints }
