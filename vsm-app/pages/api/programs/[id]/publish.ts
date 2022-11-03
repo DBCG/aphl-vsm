c8b38b31d44695b72934216ec9c2050960d26542
Vicki Strateman <vicki@vermonster.com>
2022-11-03 15:41:08 -0400

    clean up publish and release, add logging to server reqs

--- vsm-app/pages/api/programs/[id]/publish.ts
+++ vsm-app/pages/api/programs/[id]/publish.ts
@@ -13,6 +13,8 @@ export default async function handler(
   }
 
   if (req.method === 'POST') {
+    console.log('PUBLISHING...')
+    console.log('.......................................................')
     const response = await fetch(`${process.env.FHIR_CDR_URL}/$publish`, {
       method: 'POST',
       headers: {
@@ -21,6 +23,11 @@ export default async function handler(
       },
       body: req.body
     })
+
+    const json = await response.json()
+    console.log('...............................................')
+    console.log('PUBLISH RESPONSE: ')
+    console.log(json)
 
     res.send(response)
   }
