c48240a4d1b421b25094e4957ec31de811f585f4
Vicki Strateman <vicki@vermonster.com>
2022-11-04 14:34:22 -0400

    remove logs that were slowing reqs

--- vsm-app/pages/api/programs/index.ts
+++ vsm-app/pages/api/programs/index.ts
@@ -50,13 +50,10 @@ export default async function handler(
       console.log('result: ', searchResult)
 
       if (searchResult.entry) {
-        console.log('resources exist: ')
         const programs = searchResult?.entry?.map((e: any) => e?.resource)
         const json = JSON.stringify(programs)
         res.status(200).send(json)
       } else {
-        console.error('..........................................')
-        console.error('no entry in result:')
         console.error(searchResult)
         res.status(404).send([])
       } 
