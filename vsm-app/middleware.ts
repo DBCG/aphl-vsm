2269535a7b96f32ac6b37d46825a725b9d439852
Dewar Tan <dewar0019@gmail.com>
2022-11-16 20:28:28 -1000

    Fix bug in middleware with production build

--- vsm-app/middleware.ts
+++ vsm-app/middleware.ts
@@ -1,18 +1,3 @@
-import { getSession } from "next-auth/react";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function middleware(request: NextRequest) {
-  const cookie = request.headers.get("cookie"),
-    session: any = cookie
-      ? await getSession({ req: { headers: { cookie } } as any })
-      : null;
-
-  if (!session || session.response == false) {
-    return NextResponse.redirect(new URL("/api/auth/signin", request.url));
-  }
-  return NextResponse.next();
-}
-
 export const config = {
   matcher: [
     /*
@@ -24,3 +9,5 @@ export const config = {
     '/((?!api/auth|_next/static|favicon.ico).*)',
   ],
 }
+
+export { default } from "next-auth/middleware"
