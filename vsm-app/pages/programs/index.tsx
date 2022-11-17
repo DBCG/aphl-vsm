e5a990c9edc173cc9ad84f425048695a9353cc16
Dewar Tan <dewar0019@gmail.com>
2022-11-17 06:14:21 -1000

    remove console log

--- vsm-app/pages/programs/index.tsx
+++ vsm-app/pages/programs/index.tsx
@@ -303,7 +303,7 @@ const Programs: NextPage = () => {
 
 export async function getServerSideProps(context: GetSessionParams) {
   const session = await getSession(context)
-  console.log(session)
+
   if (!session) {
     return {
       redirect: {
