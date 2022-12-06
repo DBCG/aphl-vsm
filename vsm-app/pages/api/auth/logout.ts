ea922643a418a371ea6441fb78f192dd6d5f6ea4
Dewar Tan <dewar0019@gmail.com>
2022-12-05 19:37:37 -1000

    APHL-491 add comment

--- vsm-app/pages/api/auth/logout.ts
+++ vsm-app/pages/api/auth/logout.ts
@@ -9,6 +9,7 @@ export default async function handler(
   const session = <VSMSession>await getSession(res)
   if (req.method === 'GET') {
     if (session?.idToken != null) {
+      // Initiating our own idp logout here from keycloak because next-auth doesn't support it properly
       return res.redirect(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/logout?id_token_hint=${session.idToken}&post_logout_redirect_uri=${process.env.NEXTAUTH_URL}`)
     } else {
       return res.redirect(`${process.env.NEXTAUTH_URL}`)
