993ed7ef851d269fd9f5b719cf562ba5b398b809
Vicki Strateman <vicki@vermonster.com>
2022-12-13 16:55:56 -0500

    use handler structure for endpoint

--- vsm-app/pages/api/valueset/versions.ts
+++ vsm-app/pages/api/valueset/versions.ts
@@ -1,43 +1,42 @@
-import { is } from '@/helpers/is'
-import { getTerminologySource, updateLeafVsVersion } from '@/helpers/valueSetHelpers'
-import { fhirCdrClient, terminologyClient } from 'fhirClients'
+import { updateLeafVsVersion } from '@/helpers/valueSetHelpers'
+import { fhirCdrClient } from 'fhirClients'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import handler from '@/helpers/server/handler'
 
 // this endpoint needs to:
 // update the grouper valueset canonicals to point to the right valueset version
 // add + remove versions from canonicals
-export default async function handler(
+const updateLeafValueSetVersions = async (
   req: NextApiRequest,
   res: NextApiResponse
-): Promise<any> {
-  if (req.method === 'PUT') {
-    const body = await req.body
-    const bodyJson = JSON.parse(body)
-    const { vsCanonical, vsVersion, grouperIds } = bodyJson
-    const groupersToUpdate = await Promise.all(grouperIds.map((id: string) => (
-      fhirCdrClient.read({
-        resourceType: 'ValueSet',
-        id,
-      })
-    )))
+): Promise<any> => {
+  const body = await req.body
+  const bodyJson = JSON.parse(body)
+  const { vsCanonical, vsVersion, grouperIds } = bodyJson
+  const groupersToUpdate = await Promise.all(grouperIds.map((id: string) => (
+    fhirCdrClient.read({
+      resourceType: 'ValueSet',
+      id,
+    })
+  )))
 
-    const updatedGroupers = groupersToUpdate?.map(
-      (grouperVs: fhir4.ValueSet) => updateLeafVsVersion(grouperVs, vsCanonical, vsVersion)
-    )
+  const updatedGroupers = groupersToUpdate?.map(
+    (grouperVs: fhir4.ValueSet) => updateLeafVsVersion(grouperVs, vsCanonical, vsVersion)
+  )
 
-    const resultFromUpdate = await Promise.all(updatedGroupers.map((grouperVs: fhir4.ValueSet) => (
-      fhirCdrClient.update({
-        resourceType: 'ValueSet',
-        id: grouperVs.id,
-        body: grouperVs
-      })
-    )))
+  await Promise.all(updatedGroupers.map((grouperVs: fhir4.ValueSet) => (
+    fhirCdrClient.update({
+      resourceType: 'ValueSet',
+      id: grouperVs.id,
+      body: grouperVs
+    })
+  )))
 
-    res.status(200).json({ message: 'Update valueset versions completed', grouperIds, vsCanonical })
-
-
-  } else {
-    console.error(`Method '${req.method} not supported.'`)
-    res.status(405).json({ error: 'Method not allowed.' })
-  }
+  res.status(200).json({ message: 'Update valueset versions completed', grouperIds, vsCanonical })
 }
+
+export default handler({
+  PUT: {
+    action: updateLeafValueSetVersions
+  }
+})
