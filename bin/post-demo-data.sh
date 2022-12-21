7df19f169475a3c2ca81e0d05baf50255979d1e2
Vicki Strateman <vicki@vermonster.com>
2022-12-21 15:39:30 -0500

    remove unmaintained script

--- bin/post-demo-data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# adds all resources in the demo-data folder as arguments to the load-data program
-# by default, this will POST to the HAPI dev :8080/fhir endpoint
-${DIR}/load-data.sh ${DIR}/../documentation/demo-data/\*
