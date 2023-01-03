341e61faae559413a4d29f38f84556a717b0f919
Dewar <dewar0019@gmail.com>
2023-01-02 20:17:18 -1000

    APHL-494 manifest editor (#53)
    * APHL-494 manifest editor
    
    * decodeURI incase of encoded string

---
+++ vsm-app/components/ManifestDetailTable.tsx
@@ -0,0 +1,66 @@
+import DataTable from 'react-data-table-component'
+import { Button } from '@/components/buttons/Button'
+
+
+export interface ManifestData {
+  system: string,
+  version: string,
+}
+
+interface ManifestDataMap {
+  [key: string]: string[];
+}
+
+const prepData = (data: ManifestDataMap) => {
+  const preparedData = [] as ManifestData[]
+  Object.entries(data).forEach(([system, value]) => {
+    value.forEach((version) => preparedData.push({system, version}))
+  })
+  return preparedData
+}
+
+const ManifestDetailTable = ({ deleteFn = false, data = {}, customStyles }: any) => {
+  const preppedData = prepData(data)
+  const columns = [
+    {
+      name: 'Delete',
+      omit: !deleteFn,
+      cell: (removeVersion: ManifestData) => {
+        return (
+          <Button
+            data-tag="allowRowEvents"
+            text="Delete"
+            onClick={() => deleteFn(removeVersion)}
+          />
+        );
+      },
+      sortable: true,
+      wrap: true
+    },
+    {
+      name: 'System',
+      selector: (row: ManifestData) => row.system!,
+      sortable: true,
+      wrap: true
+    },
+    {
+      name: 'Version',
+      selector: (row: ManifestData) => row.version!,
+      sortable: true,
+      wrap: true
+    },
+  ]
+  
+  return (
+    <DataTable
+      columns={columns}
+      highlightOnHover
+      customStyles={customStyles}
+      data={preppedData}
+      pagination
+      paginationPerPage={10}
+    />
+  )
+}
+
+export default ManifestDetailTable
---
+++ vsm-app/pages/api/programs/[id]/manifest.ts
@@ -0,0 +1,73 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { terminologyClient } from 'fhirClients'
+import handler from '@/helpers/server/handler'
+import { fhirCdrClient } from 'fhirClients'
+import { getExpansionParametersSystemVersion, setExpansionParameters } from '@/helpers/valueSetHelpers';
+import { getGrouperLibraryCanonical } from '@/helpers/libraryHelpers';
+
+const getManifestVersions = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+    terminologyClient.setClient('vsac')
+    const activeTerminologyClient = terminologyClient.getClient()
+  try {
+    if (req.query.url) {
+      const results = await activeTerminologyClient?.search({
+        resourceType: 'CodeSystem',
+        searchParams: {
+          system: req.query.url,
+        }
+      })
+
+      const versions = results?.entry?.map((i: fhir4.BundleEntry) => i?.resource?.version)
+      return res.status(200).json(versions)
+    }
+
+    const terminologyCapabilityStatement = await activeTerminologyClient?.capabilityStatement()
+    const availableCodeSystems = terminologyCapabilityStatement?.extension?.map((ext: fhir4.Extension) => {
+      const uri = ext?.extension?.find(({url}) => url === 'system')?.valueUri
+      const name = ext?.extension?.find(({url}) => url === 'name')?.valueString
+      return { uri, name }
+    }).filter((x: any) => x.uri && x.name)
+
+    return res.status(200).json(availableCodeSystems)
+  } catch (e) {
+    console.error('error:  ', e)
+    return res.status(400).json({ 'server-error': 'ValueSet search failed.' })
+  }
+}
+
+const updateManifest = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const grouperLibrary = await fhirCdrClient.read({
+    resourceType: 'Library',
+    id: req.query.id as string
+  }) as fhir4.Library
+
+ const manifestLibraryUrl = getGrouperLibraryCanonical(grouperLibrary)
+
+  const manifestLibrary = await fhirCdrClient.search({
+    resourceType: 'Library',
+    searchParams: {
+      url: manifestLibraryUrl as string
+    }
+  }).then (res => res?.entry?.[0]?.resource)
+
+  setExpansionParameters(manifestLibrary, req.body)
+  const updatedExpansionParameters = getExpansionParametersSystemVersion(manifestLibrary)
+
+  await fhirCdrClient.update({
+    resourceType: 'Library',
+    id: manifestLibrary.id,
+    body: manifestLibrary
+  })
+  return res.status(200).json(updatedExpansionParameters)
+}
+
+export default handler({
+  GET: { action: getManifestVersions},
+  PUT: { action: updateManifest }
+})
