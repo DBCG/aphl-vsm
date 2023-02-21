7cf8ec9a4dcd65616d8a0a164ed175e236d9af09
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-02-21 14:01:49 -0500

    [APHL-474] set TS formatter to remove semicolons

--- vsm-app/pages/programs/[id]/approve.tsx
+++ vsm-app/pages/programs/[id]/approve.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import Select, { SingleValue } from 'react-select';
-import { PageTitle } from '@/components/Typography';
-import { StyledSpan } from '.';
-import { Button } from '@/components/buttons/Button';
-import { SearchInput } from '@/components/SearchInput';
-import { StyledLabel } from '@/components/SearchInput';
-import { useGetProgramDetails } from '@/hooks/useGetProgramDetails';
-import type { NextPage } from 'next';
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import { useRouter } from 'next/router'
+import Select, { SingleValue } from 'react-select'
+import { PageTitle } from '@/components/Typography'
+import { StyledSpan } from '.'
+import { Button } from '@/components/buttons/Button'
+import { SearchInput } from '@/components/SearchInput'
+import { StyledLabel } from '@/components/SearchInput'
+import { useGetProgramDetails } from '@/hooks/useGetProgramDetails'
+import type { NextPage } from 'next'
 
 const GridContainer = styled.div`
   display: grid;
@@ -16,28 +16,28 @@ const GridContainer = styled.div`
   gap: 24px;
   max-width: 1200px;
   margin-bottom: 48px;
-`;
+`
 
 const StyledDateInput = styled.input.attrs({
   type: 'date'
 })`
   margin-bottom: 24px;
-`;
+`
 
 const Row = styled.div`
   display: flex;
   flex: 1;
   flex-direction: row;
   justify-content: flex-start;
-`;
+`
 
 const SubtitleRow = styled(Row)`
   margin-bottom: 8px;
-`;
+`
 
 const LabelStyled = styled(StyledLabel)`
   margin-bottom: 0;
-`;
+`
 
 const Col = styled.div`
   display: flex;
@@ -45,7 +45,7 @@ const Col = styled.div`
   flex-direction: column;
   height: fit-content;
   gap: 8px;
-`;
+`
 
 // http://hl7.org/fhir/R4/valueset-contact-point-system.html
 const contactOptions = {
@@ -61,30 +61,30 @@ const contactOptions = {
     display: "Email",
     validation: "email"
   }
-};
+}
 
 const artifactCommentTypes = {
   documentation: 'Documentation',
   review: 'Review',
   guidance: 'Guidance',
-};
+}
 
 interface formData {
-  approvalDate: Date;
-  endorserName: string;
-  endorserContact: string;
-  endorserContactType: keyof typeof contactOptions | '';
-  endorserContactValue: string;
-  artifactCommentType: keyof typeof artifactCommentTypes | '';
-  artifactCommentText: string;
-  artifactCommentTarget: string;
-  artifactCommentReference: string;
-  artifactCommentUser: string;
+  approvalDate: Date
+  endorserName: string
+  endorserContact: string
+  endorserContactType: keyof typeof contactOptions | ''
+  endorserContactValue: string
+  artifactCommentType: keyof typeof artifactCommentTypes | ''
+  artifactCommentText: string
+  artifactCommentTarget: string
+  artifactCommentReference: string
+  artifactCommentUser: string
 }
 
 const ApproveInfoForm: NextPage = () => {
-  const router = useRouter();
-  const programAndGrouperInfo = useGetProgramDetails(router.query.id as string);
+  const router = useRouter()
+  const programAndGrouperInfo = useGetProgramDetails(router.query.id as string)
   const [approvalFormData, setApprovalFormData] = React.useState<formData>({
     approvalDate: new Date(),
     endorserName: '',
@@ -96,54 +96,54 @@ const ApproveInfoForm: NextPage = () => {
     artifactCommentTarget: '',
     artifactCommentReference: '',
     artifactCommentUser: '',
-  });
+  })
 
   const handleApprove = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const parameterObj = createParametersObj(approvalFormData);
-    const approveEndpoint = `/api/programs/${(programAndGrouperInfo.program as fhir4.Library).id}/approve`;
+    const parameterObj = createParametersObj(approvalFormData)
+    const approveEndpoint = `/api/programs/${(programAndGrouperInfo.program as fhir4.Library).id}/approve`
     return fetch(approveEndpoint, {
       method: 'POST',
       body: JSON.stringify(parameterObj)
-    });
-  };
-  const handleFieldChange = (e: React.ChangeEvent<HTMLInputElement> | SingleValue<{ label: string, value: string; }>, fieldName: keyof formData) => {
+    })
+  }
+  const handleFieldChange = (e: React.ChangeEvent<HTMLInputElement> | SingleValue<{ label: string; value: string }>, fieldName: keyof formData) => {
     if (!e) {
-      console.error('undefined event in Approve form!');
-      return;
+      console.error('undefined event in Approve form!')
+      return
     }
     if ('label' in e) {
       // this is a React Select component changeEvent
       setApprovalFormData({
         ...approvalFormData,
         [fieldName]: e.value
-      });
+      })
     } else {
       // this is a regular input component changeEvent
       if (fieldName === 'approvalDate') {
-        const newDate = new Date(e?.target?.value || "");
+        const newDate = new Date(e?.target?.value || "")
         if (newDate.toString() !== 'Invalid Date') {
           setApprovalFormData({
             ...approvalFormData,
             [fieldName]: newDate
-          });
+          })
         }
       } else {
         setApprovalFormData({
           ...approvalFormData,
           [fieldName]: e.target.value
-        });
+        })
       }
     }
-  };
+  }
   const createParametersObj = (params: formData) => {
-    const parametersObj: fhir4.Parameters = { resourceType: "Parameters" };
-    parametersObj.parameter = [];
+    const parametersObj: fhir4.Parameters = { resourceType: "Parameters" }
+    parametersObj.parameter = []
     for (const key in params) {
       if (key === 'approvalDate') {
         parametersObj.parameter.push({
           name: 'approvalDate',
           valueDate: approvalFormData.approvalDate.toISOString()
-        });
+        })
       } else if (key === 'endorserName' && approvalFormData.endorserName) {
         parametersObj.parameter.push({
           name: 'endorser',
@@ -154,20 +154,20 @@ const ApproveInfoForm: NextPage = () => {
               system: approvalFormData.endorserContactType || undefined
             }] : undefined
           }
-        });
+        })
       } else {
         // stop TS worrying about approvalDate being a Date type
-        const val = params[key as keyof Omit<formData, 'approvalDate'>];
+        const val = params[key as keyof Omit<formData, 'approvalDate'>]
         if (val) {
           parametersObj.parameter.push({
             name: key,
             valueString: val
-          });
+          })
         }
       }
     }
-    return parametersObj;
-  };
+    return parametersObj
+  }
   return (
     <>
       <Row>
@@ -244,7 +244,7 @@ const ApproveInfoForm: NextPage = () => {
         />
       </Row>
     </>
-  );
-};
+  )
+}
 
-export default ApproveInfoForm;
+export default ApproveInfoForm
