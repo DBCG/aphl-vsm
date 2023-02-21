b33b50a222de53b2db032a8c9d4395d5a0bccd14
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-02-21 15:29:59 -0500

    [APHL-474] updated with new image name

--- bin/run-fhir-server.sh
+++ bin/run-fhir-server.sh
@@ -3,11 +3,12 @@
 set -e
 
 CONTAINER_NAME="cqf-ruler-vsm"
+IMAGE_NAME="alphora/cqf-ruler:cqf-ruler-vsm-approve:latest"
 MATCHING_CONTAINERS=$(docker container ls -a | grep "$CONTAINER_NAME" | wc -l)
 
 # if container does not already exist, make it
 if (( $MATCHING_CONTAINERS < 1)); then
-  docker run -e "hapi.fhir.server_address=http://localhost:8082/fhir" -e "hapi.fhir.fhir_version=R4" -e "hapi.fhir.cql.cql_logging_enabled=true" -p 8082:8080 --name cqf-ruler-vsm alphora/cqf-ruler:vsm_draft_operation
+  docker run -e "hapi.fhir.server_address=http://localhost:8082/fhir" -e "hapi.fhir.fhir_version=R4" -e "hapi.fhir.cql.cql_logging_enabled=true" -p 8082:8080 --name $CONTAINER_NAME $IMAGE_NAME
 else
-  docker start cqf-ruler-vsm
+  docker start $CONTAINER_NAME
 fi
