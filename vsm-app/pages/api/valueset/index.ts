0700bf7ee1adb8b9ff46c85265b85f772c5e0d70
Vicki Strateman <vicki@vermonster.com>
2023-03-13 16:11:58 -0400

    fix broken valueset add

--- vsm-app/pages/api/valueset/index.ts
+++ vsm-app/pages/api/valueset/index.ts
@@ -7,7 +7,11 @@ import { terminologyClient } from 'fhirClients'
 import { terminologyServerEndpoints } from 'fhirClientOptions'
 import { is } from '@/helpers/is'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<any> {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<any> {
+
   // get ValueSets by id
   if (req.method === 'GET') {
     try {
@@ -18,8 +22,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       console.error(e)
       res.status(400).json({ error: 'Loading ValueSets failed' })
     }
-  }
-  if (req.method === 'PUT') {
+  } if (req.method === 'PUT') {
     const body = await req.body
     const bodyJson = JSON.parse(body)
 
@@ -27,26 +30,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     let vsToUpdate
 
     // check fhir server first to see if we already have the selected valueSets
-    const serverResponses = await Promise.allSettled(
-      bodyJson?.selectedValueSets?.map((item: any) =>
-        fhirCdrClient.search({
-          resourceType: 'ValueSet',
-          searchParams: {
-            url: item.url,
-            version: item.version
-          }
-        })
-      )
+    const serverResponses = await Promise.allSettled(bodyJson?.selectedValueSets?.map((item: any) => (
+      fhirCdrClient.search({
+        resourceType: 'ValueSet',
+        searchParams: {
+          url: item.url,
+          version: item.version
+        }
+      })
     )
+    ))
 
     const existingVSetBundles = serverResponses
-      ?.map((item) => item?.status === 'fulfilled' && item?.value)
-      ?.filter((x) => !!x) as fhir4.Bundle[]
+      ?.map(item => item?.status === 'fulfilled' && item?.value)
+      ?.filter(x => x) as fhir4.Bundle[]
 
-    const filteredVSets = existingVSetBundles?.filter((x) => !!x)?.map((item) => item?.entry?.[0]?.resource) as fhir4.ValueSet[]
+    const filteredVSets = existingVSetBundles
+      ?.filter(x => x)
+      ?.map(item => item?.entry?.[0]?.resource) as fhir4.ValueSet[]
 
     for (const selectedVS of bodyJson.selectedValueSets) {
-      const matchingValueSetInCQF = filteredVSets?.find((vs) => vs?.url === selectedVS?.url && vs?.version === selectedVS?.version)
+      const matchingValueSetInCQF = filteredVSets?.find(vs => vs?.url === selectedVS?.url && vs?.version === selectedVS?.version)
       // valueset already exists in our server, don't need to call other terminology server
       if (matchingValueSetInCQF) {
         vsToUpdate = matchingValueSetInCQF
@@ -56,16 +60,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           terminologyClient.setClient(bodyJson.selectedTerminologyServer)
           const terminologyClientInstance = terminologyClient.getClient()
           if (terminologyClientInstance) {
+
             // get all matching valuesets
             // vsac doesn't support _sort so doing this broader search + sorting below
             const allAvailableMatches = await terminologyClientInstance.search({
               resourceType: 'ValueSet',
               searchParams: {
-                url: selectedVS.url
+                url: selectedVS.url.split('-')[0]
               }
             })
 
             if (allAvailableMatches.entry) {
+              console.log('found matches in vsac:', allAvailableMatches.entry);
+
               // sorting here because we cannot use _sort on VSAC server -- not supported
               const orderedMatchingVSets = allAvailableMatches.entry
                 .map((e: fhir4.BundleEntry) => e.resource)
@@ -77,9 +84,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
               })
 
               if (is.valueSet(matchingVSetFromRemoteServer)) {
-                const vsUrl = terminologyServerEndpoints?.find(
-                  (grp) => grp.value.title.toLowerCase() === bodyJson.selectedTerminologyServer.toLowerCase()
-                )?.value?.url
+                const vsUrl = terminologyServerEndpoints
+                  ?.find(grp => grp.value.title.toLowerCase() === bodyJson.selectedTerminologyServer.toLowerCase())
+                  ?.value?.url
 
                 if (vsUrl) {
                   // add authoritativeSource extension
@@ -88,13 +95,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 }
 
                 vSetsToUpdate.push({ method: 'POST', valueSet: matchingVSetFromRemoteServer })
+
               } else {
                 console.error('no match found')
               }
+            } else {
+              console.log('no matches in vsac:', allAvailableMatches.entry);
             }
+
           } else {
             throw new Error('Terminology client is not defined')
           }
+
         } catch (e) {
           console.error('error 2: ', e)
         }
@@ -106,64 +118,65 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const valueSetItemsToUpdate = vSetsToUpdate?.map((vs) => {
       // @ts-ignore-next-line
       const updatedVs = updateConditions(vs.valueSet, bodyJson.selectedConditions, false)
-      return {
+      return ({
         valueSet: updatedVs,
         method: vs.method
-      }
+      })
     })
 
     try {
-      const performedUpdate = await Promise.allSettled(
-        valueSetItemsToUpdate.map(async (item) => {
-          if (item.method === 'PUT') {
-            await fhirCdrClient.update({
-              resourceType: 'ValueSet',
-              id: item.valueSet.id,
-              body: item.valueSet
-            })
-          } else {
-            await fhirCdrClient.create({
-              resourceType: 'ValueSet',
-              body: item.valueSet
-            })
-          }
-        })
-      )
+      const performedUpdate = await Promise.allSettled(valueSetItemsToUpdate.map(async (item) => {
+        if (item.method === 'PUT') {
+          await fhirCdrClient.update({
+            resourceType: 'ValueSet',
+            id: item.valueSet.id,
+            body: item.valueSet
+          })
+        } else {
+          await fhirCdrClient.create({
+            resourceType: 'ValueSet',
+            body: item.valueSet
+          })
+        }
+      }))
 
-      const failedUpdates = performedUpdate?.filter((promiseItem) => promiseItem.status === 'rejected')
+      const failedUpdates = performedUpdate?.filter(promiseItem => promiseItem.status === 'rejected')
       console.error('failed updates: ', failedUpdates)
+
     } catch (e) {
       console.error('error 3', e)
     }
 
     // get groupers
-    const groupersToUpdate = await Promise.all(
-      bodyJson.selectedGroupers.map(async (grouperItem: any) => {
-        return await fhirCdrClient.read({
-          resourceType: 'ValueSet',
-          id: grouperItem.id
-        })
+    const groupersToUpdate = await Promise.all(bodyJson.selectedGroupers.map(async (grouperItem: any) => {
+      return await fhirCdrClient.read({
+        resourceType: 'ValueSet',
+        id: grouperItem.id
       })
-    )
+    }))
 
     try {
       // this assumes grouper already has a compose/include block, will need to be updated
       // when we allow users to create groupers
-      await Promise.all(
-        groupersToUpdate.map(async (grouperVs) => {
-          const originalComposeInclude = grouperVs.compose.include[0].valueSet
-          const newValueSetCanonicals = bodyJson.selectedValueSets.map((item: any) => item.url)
-          // deduplicate with set
-          const newComposeInclude = Array.from(new Set([...originalComposeInclude, ...newValueSetCanonicals]))
-          grouperVs.compose.include[0].valueSet = newComposeInclude
+      await Promise.all(groupersToUpdate.map(async (grouperVs) => {
+        const originalComposeInclude = grouperVs.compose.include
 
-          await fhirCdrClient.update({
-            resourceType: 'ValueSet',
-            id: grouperVs.id,
-            body: grouperVs
-          })
+        const newValueSetCanonicals = bodyJson.selectedValueSets.map((item: any) => item.url.split('-')[0])
+          .filter(canonical => originalComposeInclude?.find(item => item?.valueSet?.[0] !== canonical))
+
+        const newItems = newValueSetCanonicals?.map(c => ({ valueSet: [c] }))
+
+        let newComposeInclude = [...originalComposeInclude, ...newItems]
+
+        grouperVs.compose.include = newComposeInclude
+
+        await fhirCdrClient.update({
+          resourceType: 'ValueSet',
+          id: grouperVs.id,
+          body: grouperVs
         })
-      )
+
+      }))
     } catch (e) {
       console.error('error 4: ', e)
     }
