1dd283b9bef45583e1e085ec6bf56c7a557feff8
Dewar Tan <dewar0019@gmail.com>
2023-03-15 18:07:58 -1000

    refactor backend endpoint

--- vsm-app/pages/api/programs/[id]/index.ts
+++ vsm-app/pages/api/programs/[id]/index.ts
@@ -3,87 +3,95 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import Client from 'fhir-kit-client'
 import { fhirCdrClient } from 'fhirClients'
 import { is } from '@/helpers/is'
+import handler from '@/helpers/server/handler'
 
 // this only gets the program library
-export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<any> {
-  if (req.method === 'GET') {
-    if (is.string(req?.query?.id)) {
-      try {
-        const data = await fhirCdrClient.search({
-          resourceType: 'Library',
-          searchParams: {
-            context: 'triggering-valueset-library',
-            id: req.query.id
-          }
-        })
+const retrieveProgramLibrary = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (is.string(req?.query?.id)) {
+    try {
+      const data = await fhirCdrClient.search({
+        resourceType: 'Library',
+        searchParams: {
+          context: 'triggering-valueset-library',
+          id: req.query.id
+        }
+      })
 
-        const lib = data?.entry?.map((e: any) => e?.resource)
-        res.status(200).send(lib)
-      } catch (e: any) {
-        console.error('error: ', e?.response?.data?.text)
-        res.status(400).json({ error: 'Search for program by id failed.' })
-      }
-    } else {
-      console.error('error: Invalid program ID')
+      const lib = data?.entry?.map((e: any) => e?.resource)
+      res.status(200).send(lib)
+    } catch (e: any) {
+      console.error('error: ', e?.response?.data?.text)
       res.status(400).json({ error: 'Search for program by id failed.' })
     }
-  } else if (req.method === 'PUT') {
-    try {
-      // if the user does not want to change the id of the FHIR Library
-      // simply update the values in the existing resource
-      if (req.body.status === 'active') {
-        console.error('Cannot edit an active Program Library')
-        res.status(405).send('Not allowed')
-      }
-      if (req.body.id === req.query['id']) {
-        // update the program by id
-        const response = await fhirCdrClient.update({
-          resourceType: 'Library',
-          id: req.query['id'] as string,
-          body: req.body
-        })
-        res.send(response)
-      } else {
-        // if the user wants to change the id of the Library (hence non-matching ids),
-        // create a new Library with that name, then delete the original one
-        const body = await JSON.parse(req.body)
-        await fhirCdrClient
-          .update({
-            resourceType: 'Library',
-            id: body.id as string,
-            body: req.body
-          })
-          .then(async (newLibraryData) => {
-            const { response: newLibraryResponse } = Client.httpFor(newLibraryData)
-            // return response
-            if (newLibraryResponse.ok) {
-              await fhirCdrClient
-                .delete({
-                  resourceType: 'Library',
-                  id: req.query['id'] as string
-                })
-                .then((data) => {
-                  const { response: deleteResponse } = Client.httpFor(data as any)
-                  if (deleteResponse.ok) {
-                    res.send(newLibraryData)
-                  }
-                })
-            } else {
-              console.error('failed to create new program')
-            }
-          })
-      }
-    } catch (e) {
-      console.error('ERROR: ', e)
-    }
-  } else if (req.method === 'POST') {
-    // update the program by id
-    const response = await fhirCdrClient.update({
-      resourceType: 'Library',
-      id: req.query['id'] as string,
-      body: req.body
-    })
-
-    res.send(response)
+  } else {
+    console.error('error: Invalid program ID')
+    res.status(400).json({ error: 'Search for program by id failed.' })
   }
 }
+
+const createProgramLibrary = async (req: NextApiRequest, res: NextApiResponse) => {
+  // update the program by id
+  const response = await fhirCdrClient.update({
+    resourceType: 'Library',
+    id: req.query['id'] as string,
+    body: req.body
+  })
+  res.send(response)
+}
+
+const updateProgramLibrary = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    // if the user does not want to change the id of the FHIR Library
+    // simply update the values in the existing resource
+    if (req.body.status === 'active') {
+      console.error('Cannot edit an active Program Library')
+      res.status(405).send('Not allowed')
+    }
+    if (req.body.id === req.query['id']) {
+      // update the program by id
+      const response = await fhirCdrClient.update({
+        resourceType: 'Library',
+        id: req.query['id'] as string,
+        body: req.body
+      })
+      res.send(response)
+    } else {
+      // if the user wants to change the id of the Library (hence non-matching ids),
+      // create a new Library with that name, then delete the original one
+      const body = await JSON.parse(req.body)
+      await fhirCdrClient
+        .update({
+          resourceType: 'Library',
+          id: body.id as string,
+          body: req.body
+        })
+        .then(async (newLibraryData) => {
+          const { response: newLibraryResponse } = Client.httpFor(newLibraryData)
+          // return response
+          if (newLibraryResponse.ok) {
+            await fhirCdrClient
+              .delete({
+                resourceType: 'Library',
+                id: req.query['id'] as string
+              })
+              .then((data) => {
+                const { response: deleteResponse } = Client.httpFor(data as any)
+                if (deleteResponse.ok) {
+                  res.send(newLibraryData)
+                }
+              })
+          } else {
+            console.error('failed to create new program')
+          }
+        })
+    }
+  } catch (e) {
+    console.error('ERROR: ', e)
+  }
+}
+
+export default handler({
+  GET: { action: retrieveProgramLibrary },
+  PUT: { action: updateProgramLibrary },
+  POST: { action: createProgramLibrary }
+})
