881bac6ed06e508cd3f5d60f981a300e95c8d075
Dewar Tan <dewar0019@gmail.com>
2023-04-22 10:05:17 -0400

    Add pino pretty and create file path

--- vsm-app/helpers/server/logger.ts
+++ vsm-app/helpers/server/logger.ts
@@ -1,20 +1,21 @@
 /* eslint-disable no-console */
 import * as Pino from 'pino'
+import pretty from 'pino-pretty'
 import fs from 'fs'
 
 let logger: Pino.Logger
-const logPath = process.env.LOG_PATH || '/var/log/containers/middlware.log'
+const logPath = process.env.LOG_PATH || '/var/log/containers/middleware.log'
 if (process.env.ENABLE_LOGGING?.toLowerCase() === 'true') {
+  // Check if directory is present, if not create it
+  const logDir = logPath.substring(0, logPath.lastIndexOf('/'))
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true })
+  }
   const streams = [{ stream: process.stdout }, { stream: fs.createWriteStream(logPath, { flags: 'a' }) }]
   logger = Pino.pino({}, Pino.multistream(streams))
   logger.level = process.env.LOG_LEVEL || 'debug'
 } else {
-  logger = {
-    info: console.log,
-    error: console.error,
-    debug: console.log,
-    warn: console.warn
-  } as Pino.Logger
+  logger = Pino.pino(pretty())
 }
 
 export default logger
