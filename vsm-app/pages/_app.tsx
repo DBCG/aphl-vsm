99a58716b327f50579e6835969b74dc1a6d70019
Vicki Strateman <vicki@vermonster.com>
2023-04-27 12:29:42 -0400

    ensure that emotion styles are server-rendered

--- vsm-app/pages/_app.tsx
+++ vsm-app/pages/_app.tsx
@@ -8,6 +8,14 @@ import { ToastContainer, Slide } from 'react-toastify'
 import type {} from '@mui/lab/themeAugmentation'
 import 'react-toastify/dist/ReactToastify.css'
 import { NavContextProvider } from '@/components/NavBar'
+import createEmotionCache from '../utils/createEmotionCache'
+import { CacheProvider } from '@emotion/react'
+// below: https://mui.com/material-ui/react-typography/
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 const theme = createTheme({
   components: {
     // Name of the component
@@ -22,6 +30,22 @@ const theme = createTheme({
           }
         }
       }
+    },
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: 'h2',
+          h2: 'h3',
+          h3: 'h4',
+          h4: 'h4',
+          h5: 'h4',
+          h6: 'h4',
+          subtitle1: 'h4',
+          subtitle2: 'h4',
+          body1: 'span',
+          body2: 'span'
+        }
+      }
     }
   },
   palette: {
@@ -32,7 +56,9 @@ const theme = createTheme({
   }
 })
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp({ Component, pageProps: { session, ...pageProps }, emotionCache = clientSideEmotionCache }: AppProps) {
   return (
     <SessionProvider
       session={session}
@@ -41,24 +67,26 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
       // Re-fetches session when window is focused
       refetchOnWindowFocus={true}
     >
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        draggable={false}
-        pauseOnHover
-        transition={Slide}
-      />
+      <CacheProvider value={emotionCache}>
       <NavContextProvider>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          draggable={false}
+          pauseOnHover
+          transition={Slide}
+        />
         <Scaffold>
           <ThemeProvider theme={theme}>
             <Component {...pageProps} />
           </ThemeProvider>
         </Scaffold>
       </NavContextProvider>
+      </CacheProvider>
     </SessionProvider>
   )
 }
--- vsm-app/pages/_document.tsx
+++ vsm-app/pages/_document.tsx
@@ -1,29 +1,49 @@
-import Document from 'next/document'
+// import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import createEmotionServer from '@emotion/server/create-instance'
+import createEmotionCache from '../utils/createEmotionCache'
+
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+  render() {
+    return (
+      <Html lang="en">
+        {/* @ts-ignore-next-line */}
+        <Head>{this.props.emotionStyleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />)
-        })
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage
 
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      }
-    } finally {
-      sheet.seal()
-    }
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          /* @ts-ignore-next-line */
+          return <App emotionCache={cache} {...props} />
+        }
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style data-emotion={`${style.key} ${style.ids.join(' ')}`} key={style.key} dangerouslySetInnerHTML={{ __html: style.css }} />
+  ))
+
+  return {
+    ...initialProps,
+    emotionStyleTags
   }
 }
