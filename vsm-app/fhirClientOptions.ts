eac49415a63d63942302524704916d4de601009b
Dewar Tan <dewar0019@gmail.com>
2023-05-09 16:36:42 -1000

    Add in missing vsac url for deployed environment

--- vsm-app/fhirClientOptions.ts
+++ vsm-app/fhirClientOptions.ts
@@ -1,9 +1,9 @@
-let terminologyServerEndpoints = [{ label: 'VSAC', value: { title: 'vsac', url: process.env.NEXT_PUBLIC_VSAC_BASE_URL } }]
+let terminologyServerEndpoints = [{ label: 'VSAC', value: { title: 'vsac', url: process.env.VSAC_BASE_URL } }]
 
 if (process.env.NEXT_PUBLIC_SHOW_TEST_TERMINOLOGY_SERVER) {
   terminologyServerEndpoints.push({
     label: 'Ontoserver (R4)',
-    value: { title: 'ontoserverR4', url: process.env.NEXT_PUBLIC_ONTOSERVER_R4_BASE_URL }
+    value: { title: 'ontoserverR4', url: process.env.ONTOSERVER_R4_BASE_URL }
   })
 }
 
