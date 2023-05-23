126016a19d0dc3e7c238a4547e2943e1936e5f38
Vicki Strateman <vicki@vermonster.com>
2023-05-22 22:08:11 -0400

    make approve comment a textarea

--- vsm-app/pages/programs/[id]/approve.tsx
+++ vsm-app/pages/programs/[id]/approve.tsx
@@ -6,6 +6,7 @@ import { PageTitle } from '@/components/Typography'
 import { StyledSpan } from '@/styles'
 import { Button } from '@/components/buttons/Button'
 import { SearchInput } from '@/components/SearchInput'
+import { TextArea } from '@/components/TextArea'
 import { StyledLabel } from '@/components/InputLabel'
 import { useGetProgramDetails } from '@/hooks/useGetProgramDetails'
 import type { NextPage } from 'next'
@@ -253,7 +254,7 @@ const ApproveInfoForm: NextPage = () => {
             options={artifactAssessmentInfoTypeOptions}
             instanceId={'commentType'}
           />
-          <SearchInput
+          <TextArea
             id="text"
             label="Text"
             value={approvalFormData.artifactCommentText}
