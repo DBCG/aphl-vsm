e95cbcc94ae00984f86422aa7e25afaf3091981c
Dewar Tan <dewar0019@gmail.com>
2023-07-31 05:57:35 -1000

    cleanup unnecessary cookie

--- bin/clear-data.sh
+++ bin/clear-data.sh
@@ -7,7 +7,6 @@ FHIR_SERVER=${1:-http://localhost:8081/fhir}
 
 curl --location "$FHIR_SERVER/\$expunge" \
 --header 'Content-Type: application/json' \
---header 'Cookie: _session_id=96c7ac4450f29cfce417b26a0f163267' \
 --data '{
   "resourceType": "Parameters",
   "parameter": [
