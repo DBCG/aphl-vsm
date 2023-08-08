b5c1fa498d57826ced52604c834e5cdfb59b75b7
Victoria Strateman <vicki@vermonster.com>
2023-08-08 11:09:23 -0400

    remove select option wrapping at small sizes

--- vsm-app/components/ProgramValueSetDetails/index.tsx
+++ vsm-app/components/ProgramValueSetDetails/index.tsx
@@ -482,13 +482,6 @@ const ProgramValueSetDetails = ({ programId, router }: ProgramValueSetDetailsPro
                 isMulti={true}
                 options={buildConditionOptions(allConditions, selectedOptions)}
                 value={selectedOptions}
-                styles={{
-                  multiValueLabel: (baseStyles, state) => ({
-                    ...baseStyles,
-                    textOverflow: 'unset',
-                    textWrap: 'wrap'
-                  })
-                }}
                 isLoading={conditionLoading && row?.canonical === conditionToUpdate?.canonical}
                 // TODO should block add if already exists
                 onChange={(e) => {
@@ -516,13 +509,6 @@ const ProgramValueSetDetails = ({ programId, router }: ProgramValueSetDetailsPro
               inputId="groups-selector"
               instanceId="groups-selector"
               isMulti
-              styles={{
-                multiValueLabel: (baseStyles, state) => ({
-                  ...baseStyles,
-                  textOverflow: 'unset',
-                  textWrap: 'wrap'
-                })
-              }}
               options={buildGroupOptions(alphabetizedGroups)}
               // @ts-ignore-next-line
               onChange={(e) => {
@@ -553,13 +539,6 @@ const ProgramValueSetDetails = ({ programId, router }: ProgramValueSetDetailsPro
                 menuPlacement={index === 0 ? 'bottom' : 'top' }
                 isClearable={false}
                 classNamePrefix="groups"
-                styles={{
-                  multiValueLabel: (baseStyles, state) => ({
-                    ...baseStyles,
-                    textOverflow: 'unset',
-                    textWrap: 'wrap'
-                  })
-                }}
                 inputId="groups-selector"
                 instanceId="groups-selector"
                 isMulti={true}
