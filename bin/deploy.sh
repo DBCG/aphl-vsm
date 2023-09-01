2bae9b2500b6321a0028a4eeacc1d51351c611dd
Dewar Tan <dewar0019@gmail.com>
2023-09-01 08:54:04 -1000

    convert namespace to var

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -33,7 +33,7 @@ for namespace in "${namespaces[@]}"; do
   fi
   values_file=$([[ "$namespace" == "vsm" ]] && echo "$k8s_dir/values.yaml" || echo "$k8s_dir/values.qa.yaml")
 
-  if helm list -n vsm | grep -q "$helm_chart_name"; then
+  if helm list -n $namespace | grep -q "$helm_chart_name"; then
     echo "Upgrading old stack ${helm_chart_name}"
     helm upgrade "$helm_chart_name" --namespace=$namespace --set tag=$TRAVIS_COMMIT $k8s_dir -f $values_file
   else
