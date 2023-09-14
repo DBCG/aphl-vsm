28cc4c89f2dec1ea0ee7df8d2312dcbd03e16b6c
Dewar Tan <dewar0019@gmail.com>
2023-09-13 16:10:28 -1000

    add missing shared function

---
+++ vsm-app/helpers/fhirResourceHelper.ts
@@ -0,0 +1,30 @@
+import { cloneDeep } from 'lodash'
+
+type FhirResource = {
+  extension?: fhir4.Extension[]
+} & fhir4.Resource
+
+const setExtension = (resource: FhirResource , url: string, valueString: string) => {
+  const clonedResource = cloneDeep(resource)
+  const newExtensionEntry = {
+    url,
+    valueString
+  }
+
+  if (clonedResource?.extension == null) {
+    clonedResource.extension = [] as fhir4.Extension[]
+  }
+  const libraryExtensionIndex = clonedResource?.extension?.findIndex((ext) => ext?.url === url)
+
+  if (libraryExtensionIndex === -1) {
+    clonedResource.extension.push(newExtensionEntry)
+  } else {
+    clonedResource.extension[libraryExtensionIndex] = newExtensionEntry
+  }
+
+  return clonedResource
+}
+
+export {
+  setExtension
+}
