941b84d5ae630a9123a73e8d1b79be205ef81e6b
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-09-28 19:53:13 -0400

    [APHL-647] new semver

--- vsm-app/utils.ts
+++ vsm-app/utils.ts
@@ -1,34 +1,34 @@
-import cloneDeep from "lodash.clonedeep"
-import { is } from "./helpers/is"
+import cloneDeep from "lodash.clonedeep";
+import { is } from "./helpers/is";
 
 // Usage: await sleep(1000);
 export const sleep = (millis: number) => {
-  return new Promise((resolve) => setTimeout(resolve, millis))
-}
+  return new Promise((resolve) => setTimeout(resolve, millis));
+};
 export const shallowEqual = (object1: any, object2: any) => {
-  const keys1 = Object.keys(object1)
-  const keys2 = Object.keys(object2)
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
   if (keys1.length !== keys2.length) {
-    return false
+    return false;
   }
   for (let key of keys1) {
     if (object1[key] !== object2[key]) {
-      return false
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 // @ts-ignore
-export const fetcher = (...args) => fetch(...args).then((res) => res.json())
+export const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 interface IncrementParams {
-  valueToIncrement: any
-  incrementType: 'major' | 'minor' | 'patch'
-  fallbackValue: string
+  valueToIncrement: any;
+  incrementType: 'major' | 'minor' | 'patch' | 'revision';
+  fallbackValue: string;
 }
 
-const incrementStringValue = (str: string) => (parseInt(str) + 1).toString()
+const incrementStringValue = (str: string) => (parseInt(str) + 1).toString();
 
 export const incrementSemver = ({
   valueToIncrement,
@@ -36,36 +36,39 @@ export const incrementSemver = ({
   fallbackValue
 }: IncrementParams) => {
   // if not a string to begin with, return fallback default
-  if (typeof valueToIncrement !== 'string') return fallbackValue
+  if (typeof valueToIncrement !== 'string') return fallbackValue;
 
   // this will return whole string if '-draft' is not there
-  const semverWithoutTag = valueToIncrement.split('-')[0]
+  const semverWithoutTag = valueToIncrement.split('-')[0];
 
   // if value is not semver format, return fallback
-  if (!is.semver(semverWithoutTag)) return fallbackValue
+  if (!is.semver(semverWithoutTag)) return fallbackValue;
 
-  let [major, minor, patch] = semverWithoutTag.split('.')
+  let [major, minor, patch, revision] = semverWithoutTag.split('.');
 
   switch (incrementType) {
     case 'major':
-      major = incrementStringValue(major)
-      break
+      major = incrementStringValue(major);
+      break;
     case 'minor':
-      minor = incrementStringValue(minor)
-      break
+      minor = incrementStringValue(minor);
+      break;
     case 'patch':
-      patch = incrementStringValue(patch)
+      patch = incrementStringValue(patch);
+      break;
+    case 'revision':
+      patch = incrementStringValue(revision);
   }
-  return `${major}.${minor}.${patch}`
-}
+  return `${major}.${minor}.${patch}.${revision}`;
+};
 
-export const removeDraftFromVersionString = (version: string) => version.replace('-draft', '')
+export const removeDraftFromVersionString = (version: string) => version.replace('-draft', '');
 
 export const updateResourceVersion = (
   resource: fhir4.Library | fhir4.ValueSet | fhir4.PlanDefinition,
   newVersion: string
 ) => {
-  const clonedResource = cloneDeep(resource)
-  clonedResource.version = newVersion
-  return clonedResource
-}
+  const clonedResource = cloneDeep(resource);
+  clonedResource.version = newVersion;
+  return clonedResource;
+};
