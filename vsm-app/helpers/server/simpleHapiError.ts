3507cde8adcc523467d13bf04a749c5b305e7b5b
Victoria Strateman <vicki@vermonster.com>
2023-10-02 10:33:41 -0400

    update error logging to be more functional

--- vsm-app/helpers/server/simpleHapiError.ts
+++ vsm-app/helpers/server/simpleHapiError.ts
@@ -1,28 +1,33 @@
 import logger from '@/helpers/server/logger'
-import { HapiError } from '@/types/hapiError'
+import { FormattedHapiError, HapiError } from '@/types/hapiError'
 import { is } from '@/helpers/is'
 
+const formatHapiError = (e: HapiError, location?: string): FormattedHapiError => {
+  const status = e?.response?.status
+  const severity = e?.response?.data?.issue?.[0]?.severity
+  const code = e?.response?.data?.issue?.[0]?.code
+  const diagnostics = e?.response?.data?.issue?.[0]?.diagnostics
+
+  const error = ({
+    location,
+    status,
+    severity,
+    code,
+    diagnostics
+  })
+
+  return error
+}
+
 const logSimpleHapiError = (e: HapiError | any, location?: string): void => {
-  console.log('e: ', JSON.stringify(e));
 
   if (is.hapiError(e)) {
-    const status = e?.response?.status
-    const severity = e?.response?.data?.issue?.[0]?.severity
-    const code = e?.response?.data?.issue?.[0]?.code
-    const diagnostics = e?.response?.data?.issue?.[0]?.diagnostics
-
-    const error = ({
-      location,
-      status,
-      severity,
-      code,
-      diagnostics
-    })
-    logger.error(error)
+    const formattedError = formatHapiError(e, location)
+    logger.error(formattedError)
   } else {
     logger.error(`Error not from HAPI: , ${e}`)
     logger.error(`Location: ${location}`)
   }
 }
 
-export { logSimpleHapiError }
+export { logSimpleHapiError, formatHapiError }
