be78a18f67633e4b2dd2e30f8174aa0a08219479
Victoria Strateman <vicki@vermonster.com>
2023-10-02 21:33:04 -0400

    add some tests

--- vsm-app/utils.spec.ts
+++ vsm-app/utils.spec.ts
@@ -5,8 +5,15 @@ describe('incrementSemver', () => {
     expect(incrementSemver({
       valueToIncrement: '1.0.0',
       incrementType: 'major',
-      fallbackValue: '2.0.0'
+      fallbackValue: '3.0.0'
     })).toBe('2.0.0')
+
+    // with revision
+    expect(incrementSemver({
+      valueToIncrement: '1.0.0.0',
+      incrementType: 'major',
+      fallbackValue: '3.0.0.0'
+    })).toBe('2.0.0.0')
   })
 
   it('increments minor if well-formed', () => {
@@ -15,14 +22,45 @@ describe('incrementSemver', () => {
       incrementType: 'minor',
       fallbackValue: '2.0.0'
     })).toBe('1.1.0')
+
+    // with revision
+    expect(incrementSemver({
+      valueToIncrement: '1.0.0.3',
+      incrementType: 'minor',
+      fallbackValue: '2.0.0.1'
+    })).toBe('1.1.0.3')
   })
 
   it('increments patch if well-formed', () => {
     expect(incrementSemver({
       valueToIncrement: '1.0.0',
       incrementType: 'patch',
-      fallbackValue: '2.0.0'
+      fallbackValue: '3.0.0'
     })).toBe('1.0.1')
+
+    // with revision
+    expect(incrementSemver({
+      valueToIncrement: '1.0.0.9',
+      incrementType: 'patch',
+      fallbackValue: '3.0.0.8'
+    })).toBe('1.0.1.9')
+  })
+
+  it('increments revision if well-formed', () => {
+    expect(incrementSemver({
+      valueToIncrement: '1.0.0.9',
+      incrementType: 'revision',
+      fallbackValue: '3.0.0'
+    })).toBe('1.0.0.10')
+
+    // what happens if we want to increment revision
+    // and it doesn't exist
+    // we'd maybe want to do add 0 to the place so that it exists?
+    expect(incrementSemver({
+      valueToIncrement: '1.0.0',
+      incrementType: 'revision',
+      fallbackValue: '3.0.0'
+    })).toBe('1.0.0.0')
   })
 
   it('provides fallback if invalid format', () => {
