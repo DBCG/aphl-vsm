c870b69e776ab28a4ca41a06b741ad6cfdef0f6b
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-10-31 14:48:09 -0400

    [APHL-743] propagate errors to frontend

--- vsm-app/pages/programs/index.tsx
+++ vsm-app/pages/programs/index.tsx
@@ -15,6 +15,8 @@ import { ErrorMessage } from '@/components/ErrorMessage'
 import { StatusChip } from '@/components/data-display/StatusChip'
 import { customTableStyles } from '@/components/tables/themes'
 import { formatDateForTable } from '@/helpers/formatDates'
+import { DraftAPIResponse } from '../api/template'
+import { is } from '@/helpers/is'
 
 const Col = styled.div`
   display: flex;
@@ -41,7 +43,6 @@ export interface StatusProps {
 interface Error {
   error?: string
 }
-
 
 const Programs: NextPage = () => {
   const router = useRouter()
@@ -84,13 +85,17 @@ const Programs: NextPage = () => {
         method: 'POST',
         body: json
       })
-
       if (res?.ok) {
         setModalOpen(false)
         toggleNewCloneExists()
       } else {
-        const json = await res.json()
-        setError({ error: json.error })
+        const json = (await res.json()) as { message: string } | { error: string }
+        if ('error' in json) {
+          setError({ error: json.error })
+        } else {
+          console.error(json)
+          throw new Error(JSON.stringify(json))
+        }
       }
     } catch (e) {
       setError({ error: `Error cloning program ${programId}` })
@@ -117,14 +122,14 @@ const Programs: NextPage = () => {
       },
       {
         name: 'ID',
-        selector: (row: fhir4.Library) => row.id,
+        selector: (row: fhir4.Library) => row.id || '',
         sortable: true,
         maxWidth: '8rem',
-        wrap: true,
+        wrap: true
       },
       {
         name: 'Title',
-        selector: (row: fhir4.Library) => row.title,
+        selector: (row: fhir4.Library) => row.title || '',
         sortable: true,
         maxWidth: '15rem',
         minWidth: '10rem',
@@ -132,7 +137,7 @@ const Programs: NextPage = () => {
       },
       {
         name: 'Version',
-        selector: (row: fhir4.Library) => row.version,
+        selector: (row: fhir4.Library) => row.version || '',
         sortable: true,
         wrap: true,
         maxWidth: '8rem'
@@ -149,14 +154,14 @@ const Programs: NextPage = () => {
       },
       {
         name: 'Description',
-        selector: (row: fhir4.Library) => row.description,
+        selector: (row: fhir4.Library) => row.description || '',
         sortable: false,
         wrap: true,
         minWidth: '20rem'
       },
       {
         name: 'Steward',
-        selector: (row: fhir4.Library) => row.publisher,
+        selector: (row: fhir4.Library) => row.publisher || '',
         sortable: true,
         maxWidth: '15rem',
         minWidth: '10rem',
@@ -164,7 +169,7 @@ const Programs: NextPage = () => {
       },
       {
         name: 'Clone',
-        selector: (row: fhir4.Library) => row.name,
+        selector: (row: fhir4.Library) => row.name || '',
         sortable: false,
         omit: !can(session, 'clone'),
         wrap: true,
@@ -184,7 +189,7 @@ const Programs: NextPage = () => {
       },
       {
         name: 'Release',
-        selector: (row: fhir4.Library) => row.name,
+        selector: (row: fhir4.Library) => row.name || '',
         sortable: false,
         omit: !can(session, 'release'),
         wrap: true,
@@ -257,13 +262,11 @@ const Programs: NextPage = () => {
       <LoadingModal
         actionType="clone"
         isOpen={modalOpen}
-        handleModalAction={
-          async () => {
-            // throttle this action based on if it is already ongoing
-            if (cloneLoading) return
-            debouncedCloneProgram(progIdToClone)
-          }
-        }
+        handleModalAction={async () => {
+          // throttle this action based on if it is already ongoing
+          if (cloneLoading) return
+          debouncedCloneProgram(progIdToClone)
+        }}
         program={null}
         loading={cloneLoading}
         handleCancelModal={() => setModalOpen(false)}
@@ -283,7 +286,6 @@ const Programs: NextPage = () => {
       <ErrorMessage error={error?.error || null} />
       <DT
         data={programs}
-        // @ts-expect-error
         columns={columns}
         theme="aphl"
         pagination
