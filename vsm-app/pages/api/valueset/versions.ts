a4f2b316c720c1255df458acbd615bad44c8d016
Dewar <dewar0019@gmail.com>
2023-11-22 08:59:29 -1000

    Add retry for update failure (#201)
    * Add retry for update failure
    
    * [APHL-742] TS updates
    
    * better var name
    
    * fix ts error
    
    ---------
    
    Co-authored-by: taha.attari@smilecdr.com <taha.attari@smilecdr.com>

--- vsm-app/pages/api/valueset/versions.ts
+++ vsm-app/pages/api/valueset/versions.ts
@@ -7,6 +7,7 @@ import { is } from '@/helpers/is'
 import cloneDeep from 'lodash.clonedeep'
 import { terminologyServerEndpoints } from '@/fhirClientOptions'
 import { HandleVersionChange } from '@/components/ProgramValueSetDetails'
+import retry from '@/helpers/retryRequest'
 
 // --------------------------------------------
 // ------------ HELPER FUNCTIONS --------------
@@ -16,17 +17,20 @@ interface MatchExists {
   vsCanonical: string
   versionToFind: string
 }
-export interface updateLeafResponse { message: string; grouperIds?: String[]; vsCanonical?: string }
+export interface updateLeafResponse {
+  message: string
+  grouperIds?: String[]
+  vsCanonical?: string
+}
 
 const matchExistsInCQF = async ({ vsCanonical, versionToFind }: MatchExists): Promise<boolean> => {
   try {
-    const cqfSearchParams = {
+    const cqfSearchParams: { url: string; _sort: string; version?: string } = {
       url: vsCanonical,
       _sort: 'version'
     }
 
     if (versionToFind !== 'latest') {
-      // @ts-ignore
       cqfSearchParams.version = versionToFind
     }
 
@@ -86,8 +90,11 @@ const getLeafFromTermServer = async ({
   useContext
 }: GetLeaf): Promise<fhir4.ValueSet | undefined> => {
   try {
-    // @ts-ignore
-    terminologyClient.setClient(terminologyInfo.value.toLowerCase())
+    const client = terminologyInfo.value.toLowerCase()
+    if (!(client === 'vsac' || client === 'ontoserverR4')) {
+      throw "Invalid terminology server"
+    }
+    terminologyClient.setClient(client)
     const terminologyClientInstance = terminologyClient.getClient()!
 
     const searchParams = {
@@ -186,8 +193,7 @@ const updateLeafValueSetVersions = async (req: NextApiRequest, res: NextApiRespo
     const maybeHapiError = e?.response?.data
     // Sometimes the server lags behind and has not yet indexed the search for a created valueset
     // This is a fix to prevent the server from throwing an error when it tries to create a valueset that already exists
-    if (is.operationOutcome(maybeHapiError)
-      && maybeHapiError?.issue?.[0]?.diagnostics?.includes('already have one with resource ID:')) {
+    if (is.operationOutcome(maybeHapiError) && maybeHapiError?.issue?.[0]?.diagnostics?.includes('already have one with resource ID:')) {
       logger.warn(`ERROR: ${JSON.stringify(e)}`)
     } else {
       logger.error(`ERROR: ${JSON.stringify(e)}`)
@@ -199,11 +205,12 @@ const updateLeafValueSetVersions = async (req: NextApiRequest, res: NextApiRespo
 
   try {
     const groupersToUpdate = await Promise.all(
-      grouperIds.map((id: string) =>
-        fhirCdrClient.read({
-          resourceType: 'ValueSet',
-          id
-        }) as Promise<fhir4.ValueSet>
+      grouperIds.map(
+        (id: string) =>
+          fhirCdrClient.read({
+            resourceType: 'ValueSet',
+            id
+          }) as Promise<fhir4.ValueSet>
       )
     )
 
@@ -217,13 +224,16 @@ const updateLeafValueSetVersions = async (req: NextApiRequest, res: NextApiRespo
         resource: grouper as fhir4.ValueSet
       }))
 
-    await fhirCdrClient.transaction({
-      body: {
-        resourceType: 'Bundle',
-        type: 'transaction',
-        entry: updatedGroupers
-      }
-    })
+    await retry(() => fhirCdrClient.transaction({
+        body: {
+          resourceType: 'Bundle',
+          type: 'transaction',
+          entry: updatedGroupers
+        }
+      }),
+      3,
+      2000
+    )
 
     return res.status(200).json({ message: 'Update valueset versions completed', grouperIds, vsCanonical })
   } catch (e) {
