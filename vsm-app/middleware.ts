bcbb90cc624a2091be89d00556677b36d1d506fe
Victoria Strateman <vicki@vermonster.com>
2023-11-29 14:41:39 -0500

    iterate over middleware

--- vsm-app/middleware.ts
+++ vsm-app/middleware.ts
@@ -1,3 +1,6 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { withAuth } from 'next-auth/middleware'
+
 export const config = {
   matcher: [
     /*
@@ -10,4 +13,34 @@ export const config = {
   ]
 }
 
-export { default } from 'next-auth/middleware'
+const isJsonRoute = (pathsToSkip: string[], path: string): boolean => {
+  return Boolean(pathsToSkip.find(p => !path.toLowerCase().includes(p.toLowerCase())))
+}
+
+function addJsonMiddleware(req: NextRequest): NextRequest {
+  // in case you want to avoid adding json header to some api paths
+  const pathsToSkip = ['/auth']
+  const { pathname } = req.nextUrl
+
+  const addHeader = isJsonRoute(pathsToSkip, pathname)
+    // Clone the request headers and set a new header for `application/json`
+    // in most circumstances
+    if (addHeader) {
+      const requestHeaders = new Headers(req.headers)
+      requestHeaders.set('Content-Type', 'application/json')
+      console.log('headers: ', requestHeaders)
+    }
+    console.log('req: ', req)
+    return req
+}
+
+const middleWares = [withAuth, addJsonMiddleware]
+
+export default async function middleware(req: NextRequest) {
+  let reqToSend = req
+  for await (const fn of middleWares) {
+    reqToSend = await fn(req)
+  }
+  return NextResponse.next()
+}
+
