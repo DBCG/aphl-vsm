1d589b64011ab59f03ed1dae1e2ce4be738f3a6d
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-11-29 16:32:07 -0500

    update-plugin-test

--- bin/deploy_plugin.sh
+++ bin/deploy_plugin.sh
@@ -9,7 +9,7 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 aws sts get-caller-identity
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
 
-# Build and push image to ECR
+# Build and push ruler image to ECR
 docker tag cqf-ruler:$TRAVIS_COMMIT ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/cqf-ruler:$TRAVIS_COMMIT
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/cqf-ruler:$TRAVIS_COMMIT
 
