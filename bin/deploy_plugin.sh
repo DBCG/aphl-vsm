66efbf1b6dd15f2d8d5410b1c0a5214fa99e0d8d
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-11-29 16:27:56 -0500

    update deployment

--- bin/deploy_plugin.sh
+++ bin/deploy_plugin.sh
@@ -10,7 +10,7 @@ aws sts get-caller-identity
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
 
 # Build and push image to ECR
-docker tag alphora/cqf-ruler:cqf-ruler-vsm
-docker push alphora/cqf-ruler:cqf-ruler-vsm
+docker tag cqf-ruler:$TRAVIS_COMMIT ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/cqf-ruler:$TRAVIS_COMMIT
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/cqf-ruler:$TRAVIS_COMMIT
 
 echo "Deployed ecr plugin!"
