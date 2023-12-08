f3f7e9301e8d77154552f53db10108de2a9ff5b3
Dewar Tan <dewar0019@gmail.com>
2023-12-08 11:07:35 -1000

    wip

--- vsm-app/pages/api/programs/[id]/details/valuesets/conditions/batch/index.ts
+++ vsm-app/pages/api/programs/[id]/details/valuesets/conditions/batch/index.ts
@@ -1,38 +1,44 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { fhirCdrClient } from 'fhirClients'
 import { is } from '@/helpers/is'
-import { updateConditions, removeConditionsFromLeaf, Condition } from '@/helpers/conditionHelpers'
+import { removeConditionsFromLeaf, Condition } from '@/helpers/conditionHelpers'
 import handler from '@/helpers/server/handler'
 import { batchEditData } from '@/components/ProgramValueSetDetails/TableActions'
 import { MultiValue } from 'react-select'
+import { setVSConditions } from '@/helpers/libraryHelpers'
 
-const handleVsetConditionUpdates = (
-  vSets: fhir4.ValueSet[],
-  action: 'add' | 'remove' | null,
-  conditions: MultiValue<Condition>
-) => {
-  if (action === 'add') {
-    const updated = vSets.map(vs => {
-      // make conditions mutable
-      return updateConditions(vs, conditions as Condition[], false)
-    })
+// const handleVsetConditionUpdates = (
+//   vSets: fhir4.ValueSet[],
+//   action: 'add' | 'remove' | null,
+//   conditions: MultiValue<Condition>
+// ) => {
+//   if (action === 'add') {
+//     const updated = vSets.forEach(vs => {
+//       // make conditions mutable
+//       return updateConditions(vs, conditions as Condition[], false)
+//     })
 
-    return updated
-  } else if (action === 'remove') {
-    // delete from conditions, only want to update those that had changes
-    return vSets.map(leaf => removeConditionsFromLeaf(leaf, conditions as Condition[])).filter(x => Boolean(x))
-  }
-}
+//     return updated
+//   } else if (action === 'remove') {
+//     // delete from conditions, only want to update those that had changes
+//     return vSets.map(leaf => removeConditionsFromLeaf(leaf, conditions as Condition[])).filter(x => Boolean(x))
+//   }
+// }
 
 const handleBatchConditionUpdate = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const body = req.body as batchEditData
+  const programId = req.query.id as string
+  let program = await fhirCdrClient.read({ resourceType: 'Library', id: programId }) as fhir4.Library
+  if (!is.library(program)) {
+    return res.status(404).send({ message: 'Program not found for updating conditions' })
+  }
 
   const leafIds = body.leafIds
   const conditionsToUpdate = body.conditionsToUpdate
   const action = body.action
 
-  const getTransactionBody = leafIds.map(id => ({
+  const getTransactionBody = leafIds.map((id) => ({
     request: {
       method: 'GET',
       url: `ValueSet/${id}`
@@ -49,42 +55,11 @@ const handleBatchConditionUpdate = async (req: NextApiRequest, res: NextApiRespo
     body: getTransactionEntry
   })
 
-  const successfulVsets = allValueSetsToUpdate
-    ?.entry
-    ?.map((e: any) => e?.resource)
-    ?.filter((item: fhir4.Resource) => is.valueSet(item))
+  const successfulVsets = allValueSetsToUpdate?.entry?.map((e: any) => e?.resource)?.filter((item: fhir4.Resource) => is.valueSet(item))
 
-
-  const updated = (handleVsetConditionUpdates(
-    successfulVsets,
-    action,
-    conditionsToUpdate
-  )?.filter(x => is.valueSet(x)) || []) as fhir4.ValueSet[]
-
-  if (!updated.length) {
-    // if nothing is updated, there's an error
-    // this will throw to the handler
-    throw Error('No Valuesets Updated')
-  }
-
-  const putTransactionBody = updated.map(updatedVs => ({
-    request: {
-      method: 'PUT',
-      url: `ValueSet/${updatedVs.id}`,
-    },
-    resource: updatedVs
-  }))
-
-  const putTransactionEntry = {
-    resourceType: 'Bundle',
-    type: 'transaction',
-    entry: putTransactionBody
-  } as fhir4.Resource & { type: 'transaction' }
-
-  await fhirCdrClient.transaction({
-    body: putTransactionEntry
+  successfulVsets.forEach((vs: fhir4.ValueSet) => {
+    program = setVSConditions(program, conditionsToUpdate as Condition[], vs.url!)
   })
-
   res.status(200).send({ message: 'success' })
 }
 
