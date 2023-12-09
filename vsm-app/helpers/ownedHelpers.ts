633c1fcbd5cb93d390cd796845552c6be3b84ae3
Victoria Strateman <vicki@vermonster.com>
2023-12-08 21:42:33 -0500

    typescript updates

--- vsm-app/helpers/ownedHelpers.ts
+++ vsm-app/helpers/ownedHelpers.ts
@@ -1,11 +1,14 @@
+import { is } from './is'
+
 const artifactIsOwned = (art: fhir4.RelatedArtifact) => {
   return Boolean(art?.type === 'composed-of' && (
     art?.extension?.find(ext => ext?.url?.endsWith('crmi-isOwned') && ext?.valueBoolean === true)
   ))
 }
 
-const getOwnedReferences = (resource: fhir4.Library | fhir4.PlanDefinition): string[] => {
-  if (!resource.relatedArtifact) return []
+const getOwnedReferences = (resource: fhir4.Library | fhir4.PlanDefinition | fhir4.ValueSet): string[] => {
+  // valuesets don't have relatedArtifact blocks
+  if (is.valueSet(resource) || !resource?.relatedArtifact) return []
   const result = resource.relatedArtifact
     // only want owned artifacts
     .filter(i => artifactIsOwned(i))
@@ -14,16 +17,23 @@ const getOwnedReferences = (resource: fhir4.Library | fhir4.PlanDefinition): str
     return result
 }
 
-const getOwnedCanonicals = (programLib, allPossibleResources) => {
-  const allCanonicals = []
+type AllPossibleRes = (fhir4.Library | fhir4.ValueSet | fhir4.PlanDefinition)[]
 
-  const ownedCanonicals = (resource: fhir4.Library | fhir4.PlanDefinition) => {
+const getOwnedCanonicals = (
+  programLib: fhir4.Library,
+  allPossibleResources: AllPossibleRes
+) => {
+  const allCanonicals = [] as string[]
+
+  const ownedCanonicals = (resource: fhir4.Library | fhir4.PlanDefinition | fhir4.ValueSet) => {
     const refs = getOwnedReferences(resource)
     refs.forEach(canonical => {
       const [url] = canonical.split('|')
       allCanonicals.push(url)
       const matchFromBatch = allPossibleResources.find(res => res.url === url)
-      ownedCanonicals(matchFromBatch)
+      if (matchFromBatch) {
+        ownedCanonicals(matchFromBatch)
+      }
     })
   }
 
