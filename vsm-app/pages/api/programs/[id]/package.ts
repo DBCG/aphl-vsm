d3d526ae1a6dffbdcb1839b576834f41c44cec6d
Dewar <dewar0019@gmail.com>
2023-12-21 11:13:28 -1000

    Aphl 756 export ersd v1 (#207)
    * # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    [APHL-742] TS updates
    
    # The commit message #2 will be skipped:
    
    # better var name
    
    * wip
    
    * fix bugs with v1 export
    
    * fix fe modal not closing
    
    * refactor and add checkpoints
    
    * add filename to toast error message
    
    ---------
    
    Co-authored-by: taha.attari@smilecdr.com <taha.attari@smilecdr.com>

--- vsm-app/pages/api/programs/[id]/package.ts
+++ vsm-app/pages/api/programs/[id]/package.ts
@@ -2,39 +2,60 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import handler from '@/helpers/server/handler'
 import { fhirCdrClient } from 'fhirClients'
 import { logSimpleError } from '@/helpers/server/simpleHapiError'
-export type expectedPackageBody = { parameters: fhir4.Parameters; json: boolean }
+import logger from '@/helpers/server/logger'
+
+export type expectedPackageBody = { parameters: fhir4.Parameters; json: boolean; useV2: boolean }
+
 // this generates a collection Bundle containing all the resources needed to load the artifact and dependencies
 // optionally returns in XML
-const crmi_package = async (req: NextApiRequest, res: NextApiResponse<fhir4.Bundle | string | { error: string }>): Promise<void> => {
-  const { parameters, json } = req.body || {} as expectedPackageBody
+const crmiPackage = async (req: NextApiRequest, res: NextApiResponse<fhir4.Bundle | string | { error: string }>): Promise<void> => {
+  const { data, planDefinition } = req.body || ({} as expectedPackageBody)
+  const { parameters, json, useV2 } = data
   try {
-    if (json) {
-      const response = (await fhirCdrClient.operation({
-        name: '$package',
-        resourceType: 'Library',
-        id: req.query.id as string,
-        method: 'POST',
-        input: parameters
-      })) as fhir4.Bundle
-      res.send(response)
-    } else {
-      const response = await fetch(
-        `${fhirCdrClient.baseUrl}/Library/${req.query.id as string}/$package?_format=application/fhir+xml`,
-        {
-          body: JSON.stringify(parameters),
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/fhir+json',
-            // should be Basic Auth creds
-            ...fhirCdrClient.customHeaders
-          }
-        }).then(data => {
-          if (data.ok) {
-            return data.text()
-          } else {
-            throw new Error('Server error')
-          }
+    let format = json || !useV2 ? 'json' : 'xml' // default to json for v1 as we need bundle to be used in v1 export
+    const response = await fetch(`${fhirCdrClient.baseUrl}/Library/${req.query.id as string}/$package?_format=${format}`, {
+      body: JSON.stringify(parameters),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/fhir+json',
+        // should be Basic Auth creds
+        ...fhirCdrClient.customHeaders
+      }
+    }).then((r) => (json || !useV2 ? r.json() : r.text()))
+
+    if (!useV2) {
+      format = json ? 'json' : 'xml' // reset to actual format for v1
+      logger.info('Generating v2 to v1 transform for download')
+      const planDefResourceIndex = response.entry?.findIndex((e: fhir4.BundleEntry) => e.resource?.resourceType === 'PlanDefinition')
+      if (planDefResourceIndex === undefined || planDefResourceIndex === -1) {
+        response.entry.push({
+          fullUrl: planDefinition.url,
+          resource: planDefinition
         })
+      } else {
+        response.entry[planDefResourceIndex].resource = planDefinition
+      }
+
+      const v1Response = await fetch(`${fhirCdrClient.baseUrl}/$ersd-v2-to-v1-transform?_format=${format}`, {
+        body: JSON.stringify({
+          resourceType: 'Parameters',
+          parameter: [
+            {
+              name: 'bundle',
+              resource: response
+            }
+          ]
+        }),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/fhir+json',
+          // should be Basic Auth creds
+          ...fhirCdrClient.customHeaders
+        }
+      }).then((r) => (json ? r.json() : r.text()))
+
+      res.send(v1Response)
+    } else {
       res.send(response)
     }
   } catch (error: any) {
@@ -46,7 +67,7 @@ const crmi_package = async (req: NextApiRequest, res: NextApiResponse<fhir4.Bund
 
 export default handler({
   POST: {
-    action: crmi_package,
+    action: crmiPackage,
     access: ['admin']
   }
 })
