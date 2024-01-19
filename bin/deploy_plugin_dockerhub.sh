e2f116a4b47502c2b72d6cd2978b2fac92e7b538
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-01-19 15:13:08 -0500

    remove stdin

--- bin/deploy_plugin_dockerhub.sh
+++ bin/deploy_plugin_dockerhub.sh
@@ -6,7 +6,7 @@ set -o pipefail
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Login to docker hub
-docker login --username ${DOCKER_USERNAME} --password-stdin ${DOCKER_PASSWORD}
+docker login --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD}
 
 # Build and push ruler image to docker hub
 docker tag cqf-ruler:$TRAVIS_COMMIT alphora/cqf-ruler:cqf-ruler-vsm-ecr
