e32c8df246980d2409c060c70f4e0d8c6f6cdd58
Taha <TahaAttari@users.noreply.github.com>
2024-01-19 15:59:40 -0500

    add new job to deploy to hub (#232)
    * add new job to deploy to hub
    
    * update permissions
    
    * update docker compose
    
    * remove stdin
    
    ---------
    
    Co-authored-by: taha.attari@smilecdr.com <taha.attari@smilecdr.com>

---
+++ bin/deploy_plugin_dockerhub.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Login to docker hub
+docker login --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD}
+
+# Build and push ruler image to docker hub
+docker tag cqf-ruler:$TRAVIS_COMMIT alphora/cqf-ruler:cqf-ruler-vsm-ecr
+docker push alphora/cqf-ruler:cqf-ruler-vsm-ecr
+
+echo "Deployed with plugin to dockerhub!"
