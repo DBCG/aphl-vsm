934a6d8c6a16b7227c541ab06023d49e14bd751c
Dewar Tan <dewar0019@gmail.com>
2024-01-24 12:00:42 -1000

    update conditions test

--- vsm-app/tests/api/conditions/index.spec.ts
+++ vsm-app/tests/api/conditions/index.spec.ts
@@ -1,5 +1,7 @@
 import handler from '@/pages/api/conditions'
+import { fhirCdrClient } from 'fhirClients'
 
+jest.mock('fhirClients')
 // Mock Auth for Setup
 jest.mock('next-auth', () => jest.fn())
 jest.mock('next-auth/next', () => ({
@@ -12,6 +14,25 @@ jest.mock('next-auth/next', () => ({
 
 describe('GET /api/conditions', () => {
   it('should get a list of conditions', async () => {
-    
+    const req = {
+      method: 'GET'
+    }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    }
+
+    fhirCdrClient.search = jest.fn()
+
+    await handler(req, res)
+    expect(fhirCdrClient.search).toHaveBeenCalledWith({
+      resourceType: 'ValueSet',
+      searchParams: {
+        url: process.env.CONDITIONS_CANONICAL as string
+      }
+    })
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.send).toHaveBeenCalled()
+
   })
 })
