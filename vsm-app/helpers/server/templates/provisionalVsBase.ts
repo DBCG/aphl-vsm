bb5a3060bffb19534e8ab4a5e5067c042394f015
Victoria Strateman <vicki@vermonster.com>
2024-02-26 11:58:21 -0500

    template files

---
+++ vsm-app/helpers/server/templates/provisionalVsBase.ts
@@ -0,0 +1,52 @@
+const provisionalVsBase = {
+  resourceType: 'ValueSet',
+  experimental: true,
+  status: 'active',
+  meta: {
+    profile: ['http://hl7.org/fhir/us/ecr/StructureDefinition/us-ph-triggering-valueset']
+  },
+  extension: [
+    {
+      url: 'http://hl7.org/fhir/StructureDefinition/valueset-author',
+      valueContactDetail: {
+        name: 'CSTE Author'
+      }
+    },
+    {
+      url: 'http://hl7.org/fhir/StructureDefinition/valueset-steward',
+      valueContactDetail: {
+        name: 'CSTE Steward'
+      }
+    }
+  ],
+  useContext: [
+    {
+      code: {
+        system: 'http://hl7.org/fhir/us/ecr/CodeSystem/us-ph-usage-context-type',
+        code: 'reporting'
+      },
+      valueCodeableConcept: {
+        coding: [
+          {
+            system: 'http://hl7.org/fhir/us/ecr/CodeSystem/us-ph-usage-context',
+            code: 'triggering'
+          }
+        ]
+      }
+    },
+    {
+      code: {
+        system: 'http://hl7.org/fhir/us/ecr/CodeSystem/us-ph-usage-context-type',
+        code: 'priority'
+      },
+      valueCodeableConcept: {
+        coding: [
+          {
+            system: 'http://hl7.org/fhir/us/ecr/CodeSystem/us-ph-usage-context',
+            code: 'routine'
+          }
+        ]
+      }
+    }
+  ]
+} as fhir4.ValueSet
