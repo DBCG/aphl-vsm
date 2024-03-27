beed397fa9e36d62a2724d5fe7bb58cce1ab9171
Victoria Strateman <vicki@vermonster.com>
2024-03-27 12:22:23 -0400

    spelling

--- vsm-app/components/EditManifestDetails/index.tsx
+++ vsm-app/components/EditManifestDetails/index.tsx
@@ -192,7 +192,7 @@ const EditManifestDetails = ({ program }: { program: fhir4.Library }) => {
       <Modal open={availableLeafValueSetCodeSystems?.length > 0} onClose={() => setAvailableLeafValueSetCodeSystems([])}>
         <Box sx={{ ...modalStyle, width: 800, flexDirection: 'column', display: 'flex' }}>
           <Typography variant="h6" component="h2" sx={{ marginBottom: '1rem' }}>
-            ValueSets in this program contain the following version(s) of CodeSystem . If you would like all of your expansions to conform to an existing version, you may select one her
+            ValueSets in this program contain the following version(s) of CodeSystem . If you would like all of your expansions to conform to an existing version, you may select one here
           </Typography>
           <DT
             data={availableLeafValueSetCodeSystems}
