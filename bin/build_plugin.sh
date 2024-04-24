849e92c8cdc68b5328b74fa352f6a01ffc8f66df
Dewar Tan <dewar0019@gmail.com>
2024-04-24 07:26:02 -1000

    move build plugin to script for ruvos deployment

---
+++ bin/build_plugin.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+mvn install -U -DskipTests=true -Dmaven.javadoc.skip=true -T 4 -B -V
+docker buildx create --use
+# Deploy to AWS
+aws sts get-caller-identity
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
+docker buildx build --platform linux/arm64/v8,linux/amd64 -t $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/cqf-ruler:$TRAVIS_COMMIT --push .
+# Deploy to Docker Hub
+docker login --username ${DOCKER_USERNAME} --password-stdin <<< $DOCKER_PASSWORD
+docker buildx build --platform linux/arm64/v8,linux/amd64 -t alphora/cqf-ruler:cqf-ruler-vsm-ecr --push .
+
+GIT_TAG=$(git tag -l --contains HEAD 2>&1)
+if [[ -n "$GIT_TAG" ]]; then
+  echo "Begin CQF Ruler image Push to Ruvos ECR"
+  export AWS_ACCESS_KEY_ID=${PROD_AWS_ACCESS_KEY_ID}
+  export AWS_SECRET_ACCESS_KEY=${PROD_AWS_SECRET_ACCESS_KEY}
+  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${PROD_AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
+  docker push ${PROD_AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/sandbox-vsm-cqfruler:${TAG}
+  echo "CQF Ruler Image Pushed Succesfully"
+fi
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -49,12 +49,12 @@ done
 GIT_TAG=$(git tag -l --contains HEAD 2>&1)
 
 if [[ -n "$GIT_TAG" ]]; then
-  echo "Begin image Push to Ruvos ECR"
+  echo "Begin VSM image Push to Ruvos ECR"
   export AWS_ACCESS_KEY_ID=${PROD_AWS_ACCESS_KEY_ID}
   export AWS_SECRET_ACCESS_KEY=${PROD_AWS_SECRET_ACCESS_KEY}
   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${PROD_AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
   docker push ${PROD_AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/sandbox-vsm-app:${TAG}
-  echo "Image Pushed Succesfully"
+  echo "VSM Image Pushed Succesfully"
 fi
 
 echo "Deployed!"
