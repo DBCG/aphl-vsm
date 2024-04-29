41c26fede4c1d35d270192cbf393e1870955ea99
Dewar Tan <dewar0019@gmail.com>
2024-04-28 16:10:17 -1000

    fix missing args for updated exception

--- ecr/src/main/java/com/ecr/FhirResourceExists.java
+++ ecr/src/main/java/com/ecr/FhirResourceExists.java
@@ -4,7 +4,7 @@ import org.hl7.fhir.exceptions.FHIRException;
 
 public class FhirResourceExists extends FHIRException {
 	// Constructor without parameters
-	public FhirResourceExists(String resourceType, String id) {
-		super("The specified entity: " + resourceType + "/" + id + " already exists.");
+	public FhirResourceExists(String resourceType, String url, String version) {
+		super("The specified entity: " + resourceType + " with " + url + " and version: " + version + " already exists.");
 	}
 }
