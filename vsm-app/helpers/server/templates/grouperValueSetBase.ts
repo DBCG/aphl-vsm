6941dc2c71245a5b1be5a554e6c57138118f093f
Victoria Strateman <vicki@vermonster.com>
2024-05-03 11:33:05 -0400

    add priority to grouper demodata, also base grouper template

--- vsm-app/helpers/server/templates/grouperValueSetBase.ts
+++ vsm-app/helpers/server/templates/grouperValueSetBase.ts
@@ -15,6 +15,22 @@ const grouperValueSetBase = {
   },
   status: "draft",
   experimental: true,
+  relatedArtifact: {
+    extension: [
+      {
+        url: "http://aphl.org/fhir/vsm/StructureDefinition/vsm-valueset-priority",
+        valueCodeableConcept: {
+          coding: [
+            {
+              system: "http://hl7.org/fhir/us/ecr/CodeSystem/us-ph-usage-context",
+              code: "routine"
+            }
+          ],
+          text: "Routine"
+        }
+      }
+    ]
+  },
   useContext: [
     {
       code: {
@@ -35,20 +51,6 @@ const grouperValueSetBase = {
           {
             system: "http://hl7.org/fhir/us/ecr/CodeSystem/us-ph-usage-context",
             code: "triggering"
-          }
-        ]
-      }
-    },
-    {
-      code: {
-        system: "http://hl7.org/fhir/us/ecr/CodeSystem/us-ph-usage-context-type",
-        code: "priority"
-      },
-      valueCodeableConcept: {
-        coding: [
-          {
-            system: "http://hl7.org/fhir/us/ecr/CodeSystem/us-ph-usage-context",
-            code: "routine"
           }
         ]
       }
