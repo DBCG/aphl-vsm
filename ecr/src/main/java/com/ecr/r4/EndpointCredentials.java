a2bf082ef59159781a2c7f59880c462004338e07
Ivan Baisi <ivan.baisi@smiledigitalhealth.com>
2024-05-17 14:07:07 -0700

    move EndpointCredentials to own file and make it public

---
+++ ecr/src/main/java/com/ecr/r4/EndpointCredentials.java
@@ -0,0 +1,56 @@
+package com.ecr.r4;
+
+import ca.uhn.fhir.model.api.annotation.Child;
+import ca.uhn.fhir.model.api.annotation.Description;
+import org.hl7.fhir.r4.model.Endpoint;
+import org.hl7.fhir.r4.model.StringType;
+
+public class EndpointCredentials extends Endpoint {
+    @Child(
+            name = "vsacUsername",
+            type = {StringType.class},
+            order = 11,
+            min = 0,
+            max = 1,
+            modifier = false,
+            summary = true
+    )
+    @Description(
+            shortDefinition = "A name that this endpoint can be identified by",
+            formalDefinition = "A friendly name that this endpoint can be referred to with."
+    )
+    protected StringType vsacUsername;
+    @Child(
+            name = "apiKey",
+            type = {StringType.class},
+            order = 12,
+            min = 0,
+            max = 1,
+            modifier = false,
+            summary = true
+    )
+    @Description(
+            shortDefinition = "A name that this endpoint can be identified by",
+            formalDefinition = "A friendly name that this endpoint can be referred to with."
+    )
+    protected StringType apiKey;
+
+    public EndpointCredentials() {
+    }
+
+    public StringType getVsacUsername() {
+        return this.vsacUsername;
+    }
+
+    public void setVsacUsername(StringType vsacUsername) {
+        this.vsacUsername = vsacUsername;
+    }
+
+    public StringType getApiKey() {
+        return this.apiKey;
+    }
+
+    public void setApiKey(StringType apiKey) {
+        this.apiKey = apiKey;
+    }
+}
