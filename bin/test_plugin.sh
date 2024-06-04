539a99d06560901230853ffea2a2b52cc4122e34
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-06-04 14:08:56 -0400

    check maven opts

--- bin/test_plugin.sh
+++ bin/test_plugin.sh
@@ -3,4 +3,5 @@ set -o pipefail
 
 cd $TRAVIS_BUILD_DIR/ecr
 export MAVEN_OPTS="-Ddebug=false -Dorg.slf4j.simpleLogger.defaultLogLevel=error"
+echo $MAVEN_OPTS
 mvn test -U -Dmaven.javadoc.skip=true -T 4 -B -V
