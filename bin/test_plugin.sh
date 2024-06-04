299a17f2e05073280b45eb82fdba5340a136502a
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-06-04 13:00:08 -0400

    test logging error

--- bin/test_plugin.sh
+++ bin/test_plugin.sh
@@ -3,4 +3,4 @@ set -o pipefail
 
 cd $TRAVIS_BUILD_DIR/ecr
 export MAVEN_SKIP_RC=true
-mvn test -U -Dmaven.javadoc.skip=true -Ddebug=false -e -T 4 -B -V
+mvn test -U -Dmaven.javadoc.skip=true -Ddebug=false -e -Dorg.slf4j.simpleLogger.defaultLogLevel=error -T 4 -B -V
