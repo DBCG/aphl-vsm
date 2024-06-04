4fb424d61a35fc07dba9e49a4874056f5f4ca356
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-06-04 17:49:27 -0400

    try moving maven opts outside script

--- bin/test_plugin.sh
+++ bin/test_plugin.sh
@@ -2,6 +2,4 @@ set -e
 set -o pipefail
 
 cd $TRAVIS_BUILD_DIR/ecr
-export MAVEN_OPTS="-Ddebug=false -Dorg.slf4j.simpleLogger.defaultLogLevel=error"
-echo $MAVEN_OPTS
 mvn test -U -Dmaven.javadoc.skip=true -T 4 -B -V
