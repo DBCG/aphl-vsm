27c4de9b3578e1bada6d9929fa37ce45aae24483
Victoria Strateman <vicki@vermonster.com>
2024-06-17 14:06:20 -0400

    add vsm-authored & vsm-provisional tags to provisional CS template

--- vsm-app/helpers/server/templates/provisionalCsBase.ts
+++ vsm-app/helpers/server/templates/provisionalCsBase.ts
@@ -1,7 +1,20 @@
 const provisionalCsBase = {
   resourceType: 'CodeSystem',
+  meta: {
+    tag: [
+      {
+        system: 'http://aphl.org/fhir/vsm/CodeSystem/vsm-workflow-codes',
+        code: 'vsm-authored'
+      },
+      {
+        system: 'http://aphl.org/fhir/vsm/CodeSystem/vsm-workflow-codes',
+        code: 'vsm-provisional'
+      }
+    ]
+  },
   version: 'PROVISIONAL',
   status: 'draft',
+  experimental: true,
   content: 'complete', // not sure abt this one? https://build.fhir.org/valueset-codesystem-content-mode.html
 } as fhir4.CodeSystem
 
