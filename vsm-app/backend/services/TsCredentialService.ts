fa4929ef95b761f2fb93d210b867a3b27f7988f7
Ivan Baisi <ivan.baisi@smiledigitalhealth.com>
2024-07-04 13:39:12 -0700

    initial backend structure

--- vsm-app/backend/services/TsCredentialService.ts
+++ vsm-app/backend/services/TsCredentialService.ts
@@ -1,29 +1,60 @@
-//use injectify for dependency injection?
-import { injectable, inject } from "inversify";
-import "reflect-metadata";
-import { FhirClient, KeyCloadClient } from "./clients";
-import { TYPES } from "./types";
+import {FhirClient} from '../clients/FhirClient'
+import {KeyCloakClient} from '../clients/KeyCloakClient'
+import {TerminologyServerCredentials} from '../model/TerminologyServerCredential'
 
+interface TsCredentialService {
+    saveCredentials(userId: string, terminologyServerUrl: string, username: string, password: string): Promise<TerminologyServerCredentials>
+    getAllCredentials(userId: string): Promise<TerminologyServerCredentials[]>
+    getCredentials(userId: string, terminologyServerUrl: string): Promise<TerminologyServerCredentials>
+    updateCredentials(userId: string, terminologyServerUrl: string, username: string, password: string): Promise<TerminologyServerCredentials>
+}
 
-// https://www.npmjs.com/package/inversify FOR DEPENDENCY INJECTION
-@injectable()
 class TsCredentialServiceImpl implements TsCredentialService {
 
-    private fhirClient: FhirClient;
-    private keyCloakClient: KeyCloadClient;
+    readonly fhirClient: FhirClient;
+    readonly keyCloakClient: KeyCloakClient;
 
     public constructor(
-	    @inject(TYPES.FhirClient) fhirClient: FhirClient,
-	    @inject(TYPES.KeyCloadClient) keyCloakClient: KeyCloadClient
+	     fhirClient: FhirClient,
+	     keyCloakClient: KeyCloakClient
     ) {
         this.fhirClient = fhirClient;
         this.keyCloakClient = keyCloakClient;
     }
 
-    public saveCredentials(userId: UUID, terminologyServerUrl: String, username: String, password: String) { // implement credentials }
-    public getCredentials(...) { // implement get credentials }
-    public updateCredentials(...) { // implement update credentials}
+    public async saveCredentials(userId: string, terminologyServerUrl: string, username: string, password: string): Promise<TerminologyServerCredentials> {
+        const endpoints = await this.fhirClient.getTerminologyServers()
+        if(endpoints.find(e => e._address == terminologyServerUrl)) {
+            return this.keyCloakClient.storeBasicAuthCreds(userId, terminologyServerUrl, username, password)
+                .then(() => {
+                    let cred: TerminologyServerCredentials = {terminologyServerUrl, username, password}
+                    return cred
+                }, () => Promise.reject("Problem storing basic auth cred"))
+        } else {
+            return Promise.reject("Trying to save credentials for unsupported terminilogy server")
+        }
+
+    }
+
+    public async getCredentials(userId: string, terminologyServerUrl: string): Promise<TerminologyServerCredentials> {
+        let cred: Promise<TerminologyServerCredentials> = this.keyCloakClient.getUserCredentials(userId, terminologyServerUrl)
+        return cred
+    }
+
+    public async getAllCredentials(userId: string): Promise<TerminologyServerCredentials[]> {
+        return this.keyCloakClient.getAllUserCredentials(userId)
+    }
+
+    public async updateCredentials(userId: string, terminologyServerUrl: string, username: string, password: string): Promise<TerminologyServerCredentials> {
+        return this.keyCloakClient.storeBasicAuthCreds(userId, terminologyServerUrl, username, password)
+                .then(() => {
+                    let cred: TerminologyServerCredentials = {terminologyServerUrl, username, password}
+                    return cred
+                }, () => Promise.reject("Problem storing basic auth cred"))
+    }
 
 }
 
-export { TsCredentialServiceImpl };
+export { TsCredentialServiceImpl };    
+export type { TsCredentialService };
+
