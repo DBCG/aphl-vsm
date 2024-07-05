88c9fa51829dc5487441c534ece322d852d56dbb
Ivan Baisi <ivan.baisi@smiledigitalhealth.com>
2024-07-05 15:22:07 -0700

    save credentials api scafold

--- vsm-app/pages/api/tscredentials/index.ts
+++ vsm-app/pages/api/tscredentials/index.ts
@@ -1,17 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import logger from '@/helpers/server/logger'
 // keep the imports to the minumum, have only the service that will have all the business logic
-import tsCredentialService from '@/backend/services/TsCredentialService'
+import { TsCredentialService } from '@/backend/services/TsCredentialService'
+import { tsCredentialService } from '@/backend/services/TsCredentialService'
+import { TerminologyServerCredentialsRequest } from '@/backend/model/TerminologyServerCredential'
+import { logSimpleError } from '@/helpers/server/simpleHapiError'
+import { TerminologyServerCredentials } from '@/backend/model/TerminologyServerCredential'
+import handler from '@/helpers/server/handler'
 
+export type SaveCredentialsApiResponse = TerminologyServerCredentials | { error: string }
 
-const saveCredentials = async (req: NextApiRequest, res: NextApiResponse<ProgramApiResponse | {}>) => {
+const saveCredentials = async (req: NextApiRequest, res: NextApiResponse<SaveCredentialsApiResponse | {}>) => {
   try {
-      credReq: SaveCredReq = handleRequest(req)
-      tsCredentialService.saveCredentials(credReq.userId, credReq.terminologyServer, credReq.username, credReq.password)
+      const credReq: TerminologyServerCredentialsRequest = handleSaveCredentialsRequest(req)
+      const creds = tsCredentialService.saveCredentials(credReq.userId, credReq.terminologyServerUrl, credReq.username, credReq.password)
+      return res.status(200).send(creds)
     }
-  } catch (e: any) {
+  catch (e: any) {
     logSimpleError(e)
-    handleError(e) // here we check on the error and return correct http codes (401 if unauthorized, 400 if ts does not exist, etc)
+    return res.status(400).json('Search for program failed.' )
+  }
+}
+
+function handleSaveCredentialsRequest(req: NextApiRequest): TerminologyServerCredentialsRequest {
+  return {
+    userId: req.query['userId'] as string,
+    terminologyServerUrl: req.query['terminologyServerUrl'] as string,
+    username: req.query['username'] as string,
+    password: req.query['password'] as string
   }
 }
 
