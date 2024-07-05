d6b2c14de7eb21184efd2ebb2ca896539cffe98e
Ivan Baisi <ivan.baisi@smiledigitalhealth.com>
2024-07-05 15:22:07 -0700

    save credentials api scafold

--- vsm-app/backend/clients/FhirClient.ts
+++ vsm-app/backend/clients/FhirClient.ts
@@ -1,13 +1,32 @@
+import { Endpoint } from "fhir/r4";
 
 interface FhirClient {
-    //readonly fhirClient: FhirClient
 
     getTerminologyServers(): Promise<fhir4.Endpoint[]>
 
 }
 
 class FhirClientImpl implements FhirClient {
+    private static instance: FhirClientImpl;
 
+    public static getInstance(): FhirClientImpl {
+        if (!this.instance) {
+            this.instance = new FhirClientImpl
+        }
+
+        return this.instance
+    }
+
+    async getTerminologyServers(): Promise<Endpoint[]> {
+        const endpoint:fhir4.Endpoint = {
+            resourceType: "Endpoint",
+            address: "http://testts.com",
+            connectionType: { code: "" },
+            payloadType: [],
+            status: "active"
+          }
+          return [endpoint]
+    }
 }
 
 export {FhirClientImpl}
