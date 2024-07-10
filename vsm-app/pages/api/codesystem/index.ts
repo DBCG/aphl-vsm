b3ab14334578750ba91265907d426f1f52e71b85
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-07-10 16:18:42 -0400

    codesystem api response type

--- vsm-app/pages/api/codesystem/index.ts
+++ vsm-app/pages/api/codesystem/index.ts
@@ -1,39 +1,29 @@
-import { fhirCdrClient, vsacFhirClient } from '@/fhirClients'
-import { CreateProvisionalVs } from '@/helpers/provisionalVsHelpers'
+import { vsacFhirClient } from '@/fhirClients'
 import handler from '@/helpers/server/handler'
 import logger from '@/helpers/server/logger'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-interface Body extends CreateProvisionalVs {
-  grouperIds: string[]
-}
-
-interface ReqInfo extends NextApiRequest {
-  body: Body
-}
 
 interface GetBody {
   systemUrl?: fhir4.CodeSystem['url']
 }
 
-interface ProvisionalReqGet  extends NextApiRequest {
+interface ProvisionalReqGet extends NextApiRequest {
   body: GetBody
 }
-
-const getCodeSystems = async (req: ProvisionalReqGet, res: NextApiResponse) => {
+export type CodeSystemCapabilityReturn = { uri?: string; name?: string }[] | { error: string }
+const getCodeSystems = async (req: ProvisionalReqGet, res: NextApiResponse<CodeSystemCapabilityReturn>) => {
   try {
 
-  const response = await vsacFhirClient.capabilityStatement()
-  const flattened = response.extension.map((r: fhir4.Extension) => r.extension)
-   const availableCs = flattened?.map((i: fhir4.BundleEntry) => {
-    return ({
-    //@ts-ignore
-    uri: i?.find(item => item?.url === 'system')?.valueUri,
-    //@ts-ignore
-    name: i?.find(item => item?.url === 'name')?.valueString,
-  })})
+    const response = await vsacFhirClient.capabilityStatement() as fhir4.CapabilityStatement
+    const flattened = response.extension?.map((r) => r.extension)
+    const availableCs = flattened?.map((i) => {
+      return ({
+        uri: i?.find(item => item?.url === 'system')?.valueUri,
+        name: i?.find(item => item?.url === 'name')?.valueString,
+      })
+    }) || []
 
-  return res.status(200).json(availableCs)
+    return res.status(200).json(availableCs)
 
   } catch (e) {
     // logger.error(e)
