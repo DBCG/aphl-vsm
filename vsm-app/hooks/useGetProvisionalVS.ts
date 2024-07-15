8b4070adb0a9298bb1ea78d972de7bf6799d2406
Victoria Strateman <vicki@vermonster.com>
2024-07-15 12:32:27 -0400

    move pattern to SWR for get prov VS

--- vsm-app/hooks/useGetProvisionalVS.ts
+++ vsm-app/hooks/useGetProvisionalVS.ts
@@ -1,48 +1,15 @@
-import { useState, useEffect } from 'react'
+import useSWR from 'swr'
+import { fetcher } from '@/utils'
 
-interface ProvVsReturn {
-  provisionalVS: fhir4.ValueSet[]
-  isVsLoading: boolean
-}
+const useGetProvisionalVS = () => {
+  const endpoint = '/api/valueset/provisional'
+  const { data, error, isLoading } = useSWR(endpoint, fetcher)
 
-const useGetProvisionalVS = (): ProvVsReturn => {
-  const [vsets, setVsets] = useState<fhir4.ValueSet[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-  useEffect(() => {
-    setIsLoading(true)
-    async function getProvisionalVS(): Promise<void> {
-
-        let endpoint = '/api/valueset/provisional'
-        // handle queries for reference url eventually
-        try {
-          const response: Response = await fetch(endpoint)
-          if (!response.ok) {
-            // send error back here for FE eventually
-            console.error('Error occurred while searching Provisional VSets')
-            setVsets([])
-          } else {
-            const json = await response.json()
-            if ('error' in json) {
-              // better handle error here
-              console.error(json)
-              setVsets([])
-            } else {
-              setVsets(json)
-            }
-          }
-        } catch (e) {
-          console.error(e)
-          setVsets([])
-        }
-      setIsLoading(false)
-    }
-    void getProvisionalVS()
-  }, [])
-
-  return ({
-    provisionalVS: vsets,
-    isVsLoading: isLoading
-  })
+  return {
+    provisionalVS: data,
+    isVsLoading: isLoading,
+    provVsError: error
+  }
 }
 
 export { useGetProvisionalVS }
