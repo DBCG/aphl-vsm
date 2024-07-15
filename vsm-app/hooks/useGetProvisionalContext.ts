c6132fe5a26a96c31171bbc0ef083e24f53968f3
Victoria Strateman <vicki@vermonster.com>
2024-07-15 12:36:51 -0400

    convert get prov context to SWR

--- vsm-app/hooks/useGetProvisionalContext.ts
+++ vsm-app/hooks/useGetProvisionalContext.ts
@@ -1,44 +1,16 @@
-import { ProvisionalsByProgram } from '@/pages/api/programs/provisional'
-import { useState, useEffect } from 'react'
+import useSWR from 'swr'
+import { fetcher } from '@/utils'
 
-interface ReturnItems {
-  provisionalContext: ProvisionalsByProgram | { error: string }
-  isContextLoading: boolean
-}
+const useGetProvisionalContext = () => {
+  const endpoint = '/api/programs/provisional'
 
-const useGetProvisionalContext = (): ReturnItems => {
-  const [provisionalContext, setProvisionalContext] = useState<ProvisionalsByProgram | { error: string }>([])
-  const [isContextLoading, setIsContextLoading] = useState(false)
-  useEffect(() => {
-    setIsContextLoading(true)
-    async function getProvisionalContext(): Promise<void> {
-        let endpoint = '/api/programs/provisional'
-        try {
-          const response: Response = await fetch(endpoint)
-          if (!response.ok) {
-            // send error back here for FE eventually
-            console.error('Error occurred while searching Provisional Context')
-            setProvisionalContext({error: 'Error Occurred' })
-          } else {
-            const json = await response.json()
-            if ('error' in json) {
-              // better handle error here
-              console.error(json)
-              setProvisionalContext({ error: 'Error occurred' })
-            } else {
-              setProvisionalContext(json)
-            }
-          }
-        } catch (e) {
-          console.error(e)
-          setProvisionalContext({ error: 'Error occurred' })
-        }
-        setIsContextLoading(false)
-    }
-    void getProvisionalContext()
-  }, [])
+  const { data, error, isLoading } = useSWR(endpoint, fetcher)
 
-  return ({ provisionalContext, isContextLoading })
+  return {
+    provisionalContext: data,
+    isContextLoading: isLoading,
+    provContextError: error
+  }
 }
 
 export { useGetProvisionalContext }
