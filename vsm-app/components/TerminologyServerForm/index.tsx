d48d5e90574074c7d54c1ee96b2b5ddbae112987
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-08-09 14:04:04 -0400

    validation and popups

--- vsm-app/components/TerminologyServerForm/index.tsx
+++ vsm-app/components/TerminologyServerForm/index.tsx
@@ -12,6 +12,7 @@ import { EndpointRequest } from '@/pages/api/endpoint'
 import { debounce } from 'lodash'
 import { FhirResource } from 'fhir/r4'
 import { ErrorMessage } from '../ErrorMessage'
+import { toast } from 'react-toastify'
 export const authenticationTypeUrl = 'http://aphl.org/fhir/vsm/StructureDefinition/vsm-endpoint-authentication-type'
 export const getAuthenticationTypeString = (extensions: fhir4.Extension[]) =>
   extensions.find((ext) => ext.url === authenticationTypeUrl)?.valueString
@@ -20,19 +21,18 @@ export const TerminologyServerForm = ({ endpoint }: { endpoint?: fhir4.Endpoint 
   const [loading, setLoading] = useState(false)
   const [addressError, setAddressError] = useState('')
   const [nameError, setNameError] = useState('')
-  const [globalError, setGlobalError] = useState('')
   const [endpointToUpdate, setEndpointToUpdate] = useState<fhir4.Endpoint>()
   const name = useRef<HTMLInputElement>(null)
   const address = useRef<HTMLInputElement>(null)
   const authenticationType = useRef<SelectInstance<{ value: string; label: string } | null>>(null)
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setGlobalError('')
+    toast.dismiss()
     e.preventDefault()
     setLoading(true)
     if (!address?.current?.value.trim()) {
       setAddressError('Error: Address cannot be empty')
     }
-    if (!address?.current?.value.trim()) {
+    if (!name?.current?.value.trim()) {
       setNameError('Error: Name cannot be empty')
     }
     if (!name?.current?.value.trim() || !address?.current?.value.trim()) {
@@ -68,6 +68,7 @@ export const TerminologyServerForm = ({ endpoint }: { endpoint?: fhir4.Endpoint 
         body: JSON.stringify(body)
       }).then((res) => res.json() as Promise<fhir4.Endpoint | FhirResource>)
       if (response?.resourceType === 'Endpoint') {
+        toast.success(`Successfully ${!!endpointToUpdate ? 'updated' : 'created'} Endpoint`)
         if (!router.query.id) {
           router.push(`/admin-tools/edit-endpoint/${response.id!}`)
         }
@@ -76,7 +77,7 @@ export const TerminologyServerForm = ({ endpoint }: { endpoint?: fhir4.Endpoint 
       }
     } catch (err: any) {
       setLoading(false)
-      setGlobalError(err?.error || 'Error updating endpoint')
+      toast.error(err?.error || typeof err === 'string' ? err : 'Error updating endpoint')
       console.error(err)
     }
   }
@@ -122,7 +123,6 @@ export const TerminologyServerForm = ({ endpoint }: { endpoint?: fhir4.Endpoint 
     <>
       <GridContainer>
         <Col>
-          <ErrorMessage error={globalError} />
           <SubtitleRow>
             <StyledSpan>{endpoint ? 'Edit' : 'Add'} Endpoint</StyledSpan>
           </SubtitleRow>
