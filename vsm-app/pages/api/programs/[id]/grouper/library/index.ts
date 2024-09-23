200f5777b965ec9ab9c501b2b11b42f2ca1435fd
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-09-23 18:29:28 -0400

    [APHL-1137][APHL-1085] add else branch in case of weird results

--- vsm-app/pages/api/programs/[id]/grouper/library/index.ts
+++ vsm-app/pages/api/programs/[id]/grouper/library/index.ts
@@ -2,11 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { fhirCdrClient } from 'fhirClients'
 import { editComposeInclude } from '@/helpers/libraryHelpers'
 import handler from '@/helpers/server/handler'
-import { is } from '@/helpers/is'
 import logger from '@/helpers/server/logger'
 import { splitCanonical } from '@/helpers/stringHelpers'
 import { artifactIsOwned } from '@/helpers/ownedHelpers'
-import { formatErrors } from '@/helpers/server/operationOutcomeHelpers'
 
 interface EditingInfo {
   action: 'remove' | 'add'
@@ -42,7 +40,7 @@ const updateGrouperLibrary = async (req: DeleteGrouperRequest, res: NextApiRespo
         id: editingInfo.vsId
       }) as Promise<fhir4.ValueSet>])
 
-  if (is.library(grouperLib) && editingInfo.action === 'remove') {
+  if (editingInfo.action === 'remove') {
     const updatedGrouperLib = editComposeInclude({
       grouperLib,
       relatedArtifact: {
@@ -98,6 +96,9 @@ const updateGrouperLibrary = async (req: DeleteGrouperRequest, res: NextApiRespo
     }) as fhir4.Bundle
 
     return res.status(200).send(updatedGrouperLib)
+  } else {
+    logger.info("'add' functionality not implemented on programs/[id]/grouper/library")
+    return res.status(400).send({ error: "'add' functionality not implemented on programs/[id]/grouper/library" })
   }
 }
 
