2dd48dc45bfd514c994df7097edb5990ca2548c7
Taha <TahaAttari@users.noreply.github.com>
2024-10-10 12:43:15 -0400

    Update condition logic to check for transitive references (#432)
    * [APHL-1122] update condition logic to check for transitive references
    
    * [APHL-1122] update tests to check for transitive references
    
    * comment cleanup
    
    * update tests
    
    * Fixed the isOwned extension url throughout
    
    * Update ownedHelpers.ts
    
    * [APHL-1122] update transitive reference check to use Map/Sets instead of list traversal
    
    * NPE
    
    ---------
    
    Co-authored-by: taha.attari@smilecdr.com <taha.attari@smilecdr.com>
    Co-authored-by: Adam Stevenson <stevenson_adam@yahoo.com>

--- ecr/src/main/java/org/opencds/cqf/ruler/r4/CaseReportingOperationProvider.java
+++ ecr/src/main/java/org/opencds/cqf/ruler/r4/CaseReportingOperationProvider.java
@@ -393,7 +393,7 @@ public class CaseReportingOperationProvider {
 			})
 			.findFirst()
 			.ifPresent(m -> {
-				KnowledgeArtifactProcessor.handleValueSetReferenceExtensions(m, retval.getEntry(), repository);
+				KnowledgeArtifactProcessor.handleValueSetReferenceExtensions(m, retval.getEntry());
 			});
 		return retval;
 	}
