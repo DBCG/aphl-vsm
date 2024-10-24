6e199ad41b952777abde103df034ed1c7d4b2160
Dewar Tan <dewar0019@gmail.com>
2024-10-24 13:18:05 -1000

    Add security headers and clean up logs

--- vsm-app/next.config.js
+++ vsm-app/next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true'
 })
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+`
 
 const nextConfig = {
   compiler: {
@@ -9,13 +21,49 @@ const nextConfig = {
   },
   reactStrictMode: true,
   compress: true,
+  async headers() {
+    const standardHeaders = [
+      {
+        key: 'Content-Security-Policy',
+        value: cspHeader.replace(/\n/g, '')
+      }
+    ]
+
+    if (process.env.PROTOCOL === 'https') {
+      standardHeaders.push({
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+      })
+    }
+
+    return [
+      {
+        source: '/(.*)',
+        headers: standardHeaders
+      },
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, Accept, Content-Length, Content-Type, Date, X-API-KEY'
+          }
+        ]
+      }
+    ]
+  },
   async redirects() {
     return [
       {
         source: '/',
         destination: '/programs',
         permanent: true
-      },{
+      },
+      {
         source: '/admin/edit-endpoint',
         destination: '/admin',
         permanent: true
