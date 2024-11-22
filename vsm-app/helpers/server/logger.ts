edf408b2b2af835e63e937821dc511a1ae0dfb8f
Dewar Tan <dewar0019@gmail.com>
2024-11-22 07:49:22 -1000

    remove commented out code

--- vsm-app/helpers/server/logger.ts
+++ vsm-app/helpers/server/logger.ts
@@ -3,27 +3,6 @@ import * as Pino from 'pino'
 import pretty from 'pino-pretty'
 import { v4 as uuidv4 } from 'uuid'
 
-// let logId: string
-
-// let logger: Pino.Logger
-// logger = Pino.pino(pretty(
-//   {
-//     ignore: 'pid,hostname,logId', // prevent logId from being duplicate in logs
-//     messageFormat: (log) => {
-//       return "id: " + log.logId + " - " + log.msg
-//     }
-//   }
-// ))
-// getLogger().level = process.env.LOG_LEVEL || 'info'
-
-// const childLogger = () => {
-//   logId = uuidv4()
-//   console.log('logId', logId)
-//   logger = getLogger().child({ logId })
-// }
-
-// export { logId, logger }
-
 class Logger {
   private static instance: Pino.Logger
   private static logId: string
