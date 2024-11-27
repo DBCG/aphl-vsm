a4ace2edab0c8e70377aff832e1280d1652d0e2c
Victoria Strateman <vicki@vermonster.com>
2024-11-27 12:26:30 -0500

    control toggle

--- vsm-app/components/Provisional/ProgramsTab.tsx
+++ vsm-app/components/Provisional/ProgramsTab.tsx
@@ -218,11 +218,11 @@ const ExpandableRowComponent = ({ data: row, session, handleClickClone, setProgr
   )
 }
 
-const ToggleComponent = ({ handleToggleRetiredSwitch, programsExist }: any) => {
+const ToggleComponent = ({ handleToggleRetiredSwitch, programsExist, showRetired }: any) => {
   if (!programsExist) return null
   return (
     <FormGroup style={{ width: '100%', alignSelf: 'flex-start', marginTop: '1rem' }}>
-      <FormControlLabel style={{ color: 'var(--theme-500)'}} control={<Switch onChange={handleToggleRetiredSwitch}/>} label="Show retired programs" />
+      <FormControlLabel style={{ color: 'var(--theme-500)'}} control={<Switch onChange={handleToggleRetiredSwitch}/>} label="Show retired programs" checked={showRetired} />
     </FormGroup>
   )
 }
@@ -767,7 +767,7 @@ const ProgramsTab: NextPage = () => {
 
       <DT
         subHeader={Boolean(programs?.length) || undefined}
-        subHeaderComponent={<ToggleComponent programsExist={Boolean(programs?.length)} handleToggleRetiredSwitch={handleToggleRetiredSwitch} />}
+        subHeaderComponent={<ToggleComponent programsExist={Boolean(programs?.length)} handleToggleRetiredSwitch={handleToggleRetiredSwitch} showRetired={showRetired} />}
         className='programs-tab-table'
         key={refreshkey}
         data={programs}
