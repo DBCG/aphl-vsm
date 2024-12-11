6e4fba1d1b98117e42d41985e69b821270861ab6
Dewar Tan <dewar0019@gmail.com>
2024-12-10 20:42:35 -1000

    Move to default config

--- vsm-app/config.ts
+++ vsm-app/config.ts
@@ -1,3 +1,4 @@
+import Queue from 'bull'
 // DB 1 is used for user cache details
 // DB 2 is used for the worker queue jobs
 // Redis configuration
@@ -6,4 +7,13 @@ const QUEUE_REDIS_URL = `redis://${REDIS_HOST}:6379/2`
 const CACHE_REDIS_URL = `redis://${REDIS_HOST}:6379/1` 
 const JOB_EXPIRATION = process.env.JOB_EXPIRATION || 86400
 
-export { QUEUE_REDIS_URL, CACHE_REDIS_URL, JOB_EXPIRATION }
+const DEFAULT_JOB_CONFIG =  {
+  removeOnComplete: {
+    age: 24 * 3600 // keep up to 24 hours
+  },
+  removeOnFail: {
+    age: 24 * 3600 // keep up to 24 hours
+  }
+} as Queue.JobOptions
+
+export { QUEUE_REDIS_URL, CACHE_REDIS_URL, JOB_EXPIRATION, DEFAULT_JOB_CONFIG }
