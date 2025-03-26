c2137db89c336c27ddbb252add316615a2c99e34
Dewar Tan <dewar0019@gmail.com>
2025-03-25 14:58:14 -1000

    adding logging

--- vsm-app/pages/api/test-terminology-endpoint.ts
+++ vsm-app/pages/api/test-terminology-endpoint.ts
@@ -4,6 +4,7 @@ import { tsCredentialService } from '@/backend/services/TsCredentialService'
 import { VSMSession } from '@/helpers/rolesHelper'
 import handler from '@/helpers/server/handler'
 import FhirClient from '@/backend/clients/FhirClient'
+import Logger from '@/helpers/server/logger'
 
 const testTermEndpoint = async (req: NextApiRequest, res: NextApiResponse, session: VSMSession) => {
   try {
@@ -31,13 +32,14 @@ const testTermEndpoint = async (req: NextApiRequest, res: NextApiResponse, sessi
       if (serverResponse?.resourceType == 'CapabilityStatement') {
         return res.status(200).json({ status: 'ok' })
       } else {
+        Logger.getLogger().error(serverResponse)
         return res.status(500).json({ error: 'Invalid terminology server credentials' })
       }
     }
     return res.status(500).json({ error: 'No terminology server' })
 
   } catch (e) {
-    console.error(e)
+    Logger.getLogger().error(e)
     return res.status(500).json({ error: 'Error occurred' })
   }
 }
